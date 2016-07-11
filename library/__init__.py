24cf4aac935b82e79134659c4ae49d82bf11f07e
Kwat <kwat.medetgul.ernar@gmail.com>
2016-07-11 11:29:22 -0700

    __init__

--- library/__init__.py
+++ library/__init__.py
@@ -1,13 +1,17 @@
-# Check dependencies and install missing ones
+"""
+Check dependencies and install missing ones
+"""
 import pip
 
 packages_installed = [pkg.key for pkg in pip.get_installed_distributions()]
 packages_needed = ['rpy2', 'numpy', 'pandas', 'scipy', 'scikit-learn', 'matplotlib', 'seaborn']
+
 for pkg in packages_needed:
     if pkg not in packages_installed:
         print('{} not found! Installing ......'.format(pkg))
         pip.main(['install', pkg])
+
 print('Using the following packages:')
 for pkg in pip.get_installed_distributions():
     if pkg.key in packages_needed:
-        print('{} v{}'.format(pkg.key, pkg.version))
+        print('\t{} v{}'.format(pkg.key, pkg.version))
