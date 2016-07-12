539f19339f56ed14ea86a21d2449f92115679348
Kwat <kwat.medetgul.ernar@gmail.com>
2016-07-12 16:55:43 -0700

    Saving refactoring status for heatmap panel
    Former-commit-id: 4c380660d09b33fc851f95b4a0cde64cd7f501ec

--- ccbal/visualize.py
+++ ccbal/visualize.py
@@ -24,10 +24,6 @@ import seaborn as sns
 # Parameters
 # ======================================================================================================================
 # Colors
-CMAP_CONTINUOUS = mpl.cm.bwr
-CMAP_BINARY = mpl.cm.Greys
-# TODO: set up categorical color map
-# CMAP_CATEGORICAL =
 WHITE = '#FFFFFF'
 SILVER = '#C0C0C0'
 GRAY = '#808080'
@@ -44,6 +40,9 @@ BLUE = '#0000FF'
 NAVY = '#000080'
 FUCHSIA = '#FF00FF'
 PURPLE = '#800080'
+CMAP_CONTINUOUS = mpl.cm.bwr
+CMAP_BINARY = sns.light_palette('black', n_colors=128, as_cmap=True)
+CMAP_CATEGORICAL = mpl.cm.Set2
 
 # Fonts
 FONT1 = {'family': 'serif',
@@ -137,6 +136,102 @@ def plot_heatmap_panel(dataframe, reference, annotation,
 
     # Clean up the layout
     fig.tight_layout()
+
+
+def plot_heatmap_panel_v2(target, df, row_annot, title, t_type="auto", sort_target=True, class_labels=None, cbar=False):
+    ncol = df.shape[1]
+    nrow = df.shape[0]
+
+    df2 = pd.DataFrame(np.zeros((nrow, ncol)))
+
+    CMAP_CONTINUOUS = mpl.cm.bwr
+    CMAP_BINARY = sns.light_palette("black", n_colors=128, as_cmap=True)
+    CMAP_CATEGORICAL = mpl.cm.Set2
+
+    f_width = np.where(ncol / 5 < 5, 5, np.where(ncol / 5 > 7, 7, ncol / 5))
+    f_height = np.where(nrow < 25, nrow / 1.5, nrow / 2.5)
+    figure_size = (f_width, f_height)
+
+    # Target pre-processing
+    if t_type == "binary" or (t_type == "auto" and len(np.unique(target.iloc[0, :]))) == 2:
+        cmap_target = CMAP_BINARY
+        (t_vmin, t_vmax) = (0, 1)
+    elif t_type == "categ" or (t_type == "auto" and len(np.unique(target.iloc[0, :])) > 2 and
+                                       len(np.unique(target.iloc[0, :])) < ncol):
+        cmap_target = CMAP_CATEGORICAL
+        (t_vmin, t_vmax) = (0, len(np.unique(target.iloc[0:]) - 1))
+    else:  # t_type = "cont"
+        cmap_target = CMAP_CONTINUOUS
+        target.iloc[0, :] = (target.iloc[0, :] - np.mean(target.iloc[0, :])) / np.std(target.iloc[0, :])
+        (t_vmin, t_vmax) = (-2.5, 2.5)
+        means = df.mean(1, skipna=True)
+        stds = df.std(1, skipna=True)
+
+    # Feature pre-processing
+    row_names = df.index
+    max_len_row = max([len(x) for x in row_names])
+    if len(np.unique(df)) == 2:
+        cmap_features = CMAP_BINARY
+        (f_vmin, f_vmax) = (0, 1)
+        df = df + 0.25
+    else:
+        cmap_features = CMAP_CONTINUOUS
+        (f_vmin, f_vmax) = (-2.5, 2.5)
+        means = df.mean(1, skipna=True)
+        stds = df.std(1, skipna=True)
+        for i in range(nrow):
+            for j in range(ncol):
+                df.values[i, j] = (df.values[i, j] - means[i]) / stds[i]
+
+    if sort_target == True:
+        t_order = list(reversed(np.argsort(target.values[0, :], kind='quicksort')))
+        target = target.reindex_axis(target.columns[t_order], axis=1)
+        df2 = df.reindex_axis(df.columns[t_order], axis=1)
+
+        # Computer locations for class labels (for binary or categorical)
+        if (t_type == "binary" or t_type == "categ") and class_labels != None:
+            boundaries = np.zeros(len(np.unique(target.iloc[0, :])))
+            locs_labels = np.zeros(len(np.unique(target.iloc[0, :])))
+            k = 0
+            for i in range(1, ncol):
+                if target.iloc[0, i] != target.iloc[0, i - 1]:
+                    boundaries[k] = i
+                    k = k + 1
+            boundaries[len(boundaries) - 1] = ncol
+            locs_labels[0] = boundaries[0] / 2
+            for k in range(1, len(locs_labels)):
+                locs_labels[k] = boundaries[k] - (boundaries[k] - boundaries[k - 1]) / 2.0
+
+    ## Initialize figure
+    fig = plt.figure(figsize=figure_size)
+
+    ax1 = plt.subplot2grid((nrow + 1, 1), (0, 0))
+
+    sns.heatmap(target, vmin=t_vmin, vmax=t_vmax, robust=True, center=None, mask=None,
+                square=False, cmap=cmap_target, linewidth=0.0, linecolor='b',
+                annot=False, fmt=None, annot_kws={}, xticklabels=False,
+                yticklabels=['  '], cbar=False)
+    ax1.text(-0.1, 0.2, target.index[0], fontsize=13, horizontalalignment='right', fontweight='bold')
+    ax1.text(ncol / 2, 1.6, title, fontsize=16, horizontalalignment='center', fontweight='bold')
+    if (t_type == "binary" or t_type == "categ") and class_labels != None:
+        for k in range(len(locs_labels)):
+            ax1.text(locs_labels[k], 0.25, class_labels[k], fontsize=13, horizontalalignment='center',
+                     fontweight='bold')
+
+    ax2 = plt.subplot2grid((nrow + 1, 1), (0, 1), rowspan=nrow)
+    sns.heatmap(df2, vmin=f_vmin, vmax=f_vmax, robust=True, center=None, mask=None,
+                square=False, cmap=cmap_features, linewidth=0.0, linecolor='b',
+                annot=False, fmt=None, annot_kws={}, xticklabels=False,
+                yticklabels=[' ' for i in range(nrow)], cbar=cbar, cbar_kws={"orientation": "horizontal"})
+    for i in range(nrow):
+        ax2.text(-0.1, nrow - i - 1 + 0.3, row_names[i], fontsize=13, horizontalalignment='right', fontweight='bold')
+        ax2.text(ncol + 0.1, nrow - i - 1 + 0.3, row_annot[i], fontsize=13, fontweight='bold')
+    ax2.text(ncol + 1, nrow + 0.3, row_annot.name, fontsize=13, fontweight='bold')
+
+    fig.tight_layout()
+    plt.show(fig)
+
+    return
 
 
 def plot_nmf_result(nmf_results, k, figsize=(25, 10), dpi=80, output_filename=None):
