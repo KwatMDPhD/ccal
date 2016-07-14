cf1057ab1138c60e81317473ecbf7b4cfd1fcf3f
Kwat <kwat.medetgul.ernar@gmail.com>
2016-07-14 16:37:54 -0700

    plot_heatmap_panel is done
    Former-commit-id: 12b48b96abe12bd2c6314bf65a0924aa877494f8

--- ccal/visualize.py
+++ ccal/visualize.py
@@ -22,6 +22,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from .support import verbose_print
+
 # ======================================================================================================================
 # Parameters
 # ======================================================================================================================
@@ -47,114 +49,62 @@ CMAP_BINARY = sns.light_palette('black', n_colors=128, as_cmap=True)
 CMAP_CATEGORICAL = mpl.cm.Set2
 
 # Fonts
-FONT1 = {'family': 'serif',
-         'color': BLACK,
-         'weight': 'bold',
-         'size': 36}
-FONT2 = {'family': 'serif',
-         'color': BLACK,
-         'weight': 'bold',
-         'size': 24}
-FONT3 = {'family': 'serif',
-         'color': BLACK,
-         'weight': 'normal',
-         'size': 16}
+FONT20_BOLD = {'family': 'arial',
+               'size': 20,
+               'color': BLACK,
+               'weight': 'bold',
+               }
+FONT16_BOLD = {'family': 'arial',
+               'size': 16,
+               'color': BLACK,
+               'weight': 'bold',
+               }
+FONT12_BOLD = {'family': 'arial',
+               'size': 12,
+               'color': BLACK,
+               'weight': 'bold',
+               }
+FONT12 = {'family': 'arial',
+          'size': 12,
+          'color': BLACK,
+          'weight': 'normal',
+          }
 
 
 # ======================================================================================================================
 # Functions
 # ======================================================================================================================
-# TODO: use reference to make colorbar
-def plot_heatmap_panel(dataframe, reference, annotation,
-                       figure_size=(30, 30), title=None,
-                       font1=FONT1, font2=FONT2, font3=FONT3):
+def plot_heatmap_panel(ref, features, scores, ref_type, title=None, filename=None):
     """
-    Plot horizonzal heatmap panels.
-    :param dataframe: pandas DataFrame (n_samples, n_features),
-    :param reference: array-like (1, n_features),
-    :param annotation: array_like (n_samples, ),
-    :param figure_size: tuple (width, height),
+    Plot a heatmap panel.
+    :param ref: pandas DataFrame (1, n_elements),
+    :param features: pandas DataFrame (n_features, n_elements),
+    :param scores:  pandas DataFrame (n_features, 1),
     :param title: str, figure title
-    :param font1: dict,
-    :param font2: dict,
-    :param font3: dict,
+    :param filename: str, file path to save the figure
     :return: None
     """
-    # Visualization parameters
-    # TODO: Set size automatically
-    figure_height = dataframe.shape[0] + 1
-    figure_width = 7
-    heatmap_left = 0
-    heatmap_height = 1
-    heatmap_width = 6
-
-    # Initialize figure
-    fig = plt.figure(figsize=figure_size)
-    # TODO: consider removing
-    # fig.suptitle(title, fontdict=font1)
-
-    # Initialize reference axe
-    # TODO: use reference as colorbar
-    ref_min = dataframe.values.min()
-    ref_max = dataframe.values.max()
-    ax_ref = plt.subplot2grid((figure_height, figure_width), (0, heatmap_left), rowspan=heatmap_height,
-                              colspan=heatmap_width)
-    if title:
-        ax_ref.set_title(title, fontdict=font1)
-    norm_ref = mpl.colors.Normalize(vmin=ref_min, vmax=ref_max)
-    mpl.colorbar.ColorbarBase(ax_ref, cmap=CMAP_CONTINUOUS, norm=norm_ref,
-                              orientation='horizontal', ticks=[ref_min, ref_max], ticklocation='top')
-    plt.setp(ax_ref.get_xticklabels(), **font2)
-
-    # Add reference annotations
-    ax_ref_ann = plt.subplot2grid((figure_height, figure_width), (0, heatmap_left + heatmap_width),
-                                  rowspan=heatmap_height, colspan=1)
-    ax_ref_ann.set_axis_off()
-    ann = '\t\t'.join(annotation).expandtabs()
-    ax_ref_ann.text(0, 0.5, ann, fontdict=font2)
-
-    # Initialie feature axe
-    for i in range(dataframe.shape[0]):
-        # Make row axe
-        ax = plt.subplot2grid((figure_height, figure_width), (i + 1, heatmap_left), rowspan=heatmap_height,
-                              colspan=heatmap_width)
-        sns.heatmap(dataframe.ix[i:i + 1, :-len(annotation)], ax=ax,
-                    vmin=ref_min, vmax=ref_max, robust=True,
-                    center=None, mask=None,
-                    square=False, cmap=CMAP_CONTINUOUS, linewidth=0, linecolor=WHITE,
-                    annot=False, fmt=None, annot_kws={},
-                    xticklabels=False, yticklabels=True,
-                    cbar=False)
-        plt.setp(ax.get_xticklabels(), **font3, rotation=0)
-        plt.setp(ax.get_yticklabels(), **font3, rotation=0)
-
-    # Add feature annotations
-    for i in range(dataframe.shape[0]):
-        ax = plt.subplot2grid((figure_height, figure_width), (i + 1, heatmap_left + heatmap_width),
-                              rowspan=heatmap_height, colspan=1)
-        ax.set_axis_off()
-        ann = '\t\t'.join(['{:.2e}'.format(n) for n in dataframe.ix[i, annotation]]).expandtabs()
-        ax.text(0, 0.5, ann, fontdict=font3)
-
-    # Clean up the layout
-    fig.tight_layout()
-
-
-def plot_heatmap_panel_v2(ref, features, scores, ref_type, fig_size=(9, 16), title=None):
-    ref_ncol, ref_nrow = ref.shape
-    features_ncol, features_nrow = features.shape
-    scores_ncol, scores_nrow = ref.shape
+    # Check data dimensions
+    ref_nrow, ref_ncol = ref.shape
+    features_nrow, features_ncol = features.shape
+    scores_nrow, scores_ncol = scores.shape
     if ref_ncol != features_ncol:
-        raise ValueError('Numbers of columns of ref and featuers mismatch.')
+        raise ValueError('Numbers of columns of ref ({}) and featuers ({}) mismatch.'.format(ref_ncol, features_ncol))
     if features_nrow != scores_nrow:
-        raise ValueError('Numbers of rows of features and scores mismatch.')
+        raise ValueError(
+            'Numbers of rows of features ({}) and scores ({}) mismatch.'.format(features_nrow, scores_nrow))
 
     # Sort ref and features
-    ref = ref.T.sort_values('Reference', ascending=False).T
+    ref = ref.T.sort_values(ref.index[0], ascending=False).T
     features = features.reindex_axis(ref.columns, axis=1)
 
     # Initialize figure
-    fig = plt.figure(figsize=fig_size)
+    if features_ncol > 30 or features_nrow > 50:
+        fig_width, fig_height = 9, 16
+    else:
+        fig_width, fig_height = 5, 10
+    fig = plt.figure(figsize=(fig_width, fig_height), dpi=900)
+    text_margin = 0.2
 
     # Set heatmap parameters for ref
     if ref_type == 'binary':
@@ -167,11 +117,11 @@ def plot_heatmap_panel_v2(ref, features, scores, ref_type, fig_size=(9, 16), tit
         ref_cmap = CMAP_CONTINUOUS
         ref_min, ref_max = -2.5, 2.5
         # Normalize continuous values
-        ref = (ref - np.mean(ref)) / np.std(ref)
+        ref = (ref - np.mean(ref.ix[0, :])) / np.std(ref.ix[0, :])
     else:
         raise ValueError('Unknown ref_type {}.'.format(ref_type))
 
-    # Set heapmap parameters for features and normalize features
+    # Set heatmap parameters for features and normalize features
     if np.unique(features).size == 2:
         features_cmap = CMAP_BINARY
         features_min, features_max = 0, 1
@@ -188,15 +138,21 @@ def plot_heatmap_panel_v2(ref, features, scores, ref_type, fig_size=(9, 16), tit
                 features.iloc[i, j] = (v - mean) / std
 
     # Plot ref
-    ax1 = plt.subplot2grid((features_nrow, 1), (0, 0))
-    sns.heatmap(ref, vmin=ref_min, vmax=ref_max, robust=True, center=None, mask=None,
-                square=False, cmap=ref_cmap, linewidth=0.0, linecolor='b',
-                annot=False, fmt=None, annot_kws={}, xticklabels=False,
-                yticklabels=[], cbar=False)
+    ref_ax = plt.subplot2grid((features_nrow, 1), (0, 0))
     if title:
-        ax1.text(features_ncol / 2, 1.5, title, fontsize=16, horizontalalignment='center', fontweight='bold')
-    ax1.text(-0.1, 0.33, ref.index[0], fontsize=13, horizontalalignment='right', fontweight='bold')
-    ax1.text(features_ncol + 0.1, 0.33, scores.columns[0], fontsize=13, horizontalalignment='left', fontweight='bold')
+        ref_ax.text(features_ncol / 2, 3, title, horizontalalignment='center', verticalalignment='bottom',
+                    **FONT20_BOLD)
+    sns.heatmap(ref, vmin=ref_min, vmax=ref_max, robust=True, center=None, mask=None,
+                square=False, cmap=ref_cmap, linewidth=0, linecolor=BLACK,
+                annot=False, fmt=None, annot_kws={}, xticklabels=False,
+                yticklabels=False, cbar=False)
+    # Plot ref texts
+    ref_ax.text(-text_margin, 0.5, ref.index[0],
+                horizontalalignment='right', verticalalignment='center',
+                **FONT12_BOLD)
+    ref_ax.text(features_ncol + text_margin, 0.5, scores.columns[0],
+                horizontalalignment='left', verticalalignment='center',
+                **FONT12_BOLD)
     if ref_type in ('binary', 'categorical'):
         # Get boundaries
         boundaries = [0]
@@ -216,23 +172,31 @@ def plot_heatmap_panel_v2(ref, features, scores, ref_type, fig_size=(9, 16), tit
             prev_b = b
         verbose_print('label_horizontal_positions: {}'.format(label_horizontal_positions))
         unique_ref_labels = np.unique(ref.values)[::-1]
+        # Plot categories
         for i, pos in enumerate(label_horizontal_positions):
-            ax1.text(pos, 1.1, unique_ref_labels[i],
-                     fontsize=13, horizontalalignment='center', fontweight='bold')
+            ref_ax.text(pos, 1, unique_ref_labels[i], horizontalalignment='center', verticalalignment='bottom',
+                        **FONT16_BOLD)
 
-    # Plot dataframe
-    ax2 = plt.subplot2grid((features_nrow, 1), (0, 1), rowspan=features_nrow)
+    # Plot features
+    features_ax = plt.subplot2grid((features_nrow, 1), (0, 1), rowspan=features_nrow)
     sns.heatmap(features, vmin=features_min, vmax=features_max, robust=True, center=None, mask=None,
-                square=False, cmap=features_cmap, linewidth=0.0, linecolor='b',
+                square=False, cmap=features_cmap, linewidth=0, linecolor=BLACK,
                 annot=False, fmt=None, annot_kws={}, xticklabels=False,
-                yticklabels=[], cbar=False)
+                yticklabels=False, cbar=False)
 
     for i, idx in enumerate(features.index):
-        ax2.text(-0.1, features_nrow - i - 0.7, idx, fontsize=13, horizontalalignment='right', fontweight='bold')
-        ax2.text(features_ncol + 0.1, features_nrow - i - 0.7, scores.iloc[i, 0], fontsize=13, fontweight='bold')
+        features_ax.text(-text_margin, features_nrow - i - 0.5, idx, horizontalalignment='right',
+                         verticalalignment='center', **FONT12)
+        features_ax.text(features_ncol + text_margin, features_nrow - i - 0.5, '{:.3e}'.format(scores.iloc[i, 0]),
+                         horizontalalignment='left', verticalalignment='center', **FONT12)
 
-    fig.tight_layout()
+    # fig.tight_layout()
     plt.show(fig)
+
+    if filename:
+        filename += '.pdf'
+        verbose_print('Saving the figure as {} ...'.format(filename))
+        fig.savefig(filename)
 
 
 def plot_nmf_result(nmf_results, k, figsize=(25, 10), dpi=80, output_filename=None):
