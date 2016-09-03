12baefb23cb4f33f7d13426069477d329e58d5ef
KwatME <kwat.medetgul.ernar@gmail.com>
2016-09-02 17:42:49 -0700

    Done refactoring.
    Former-commit-id: 0c7f1d07fbddd00212c74770c1e2d80585a5939e

--- ccal/analyze.py
+++ ccal/analyze.py
@@ -21,7 +21,6 @@ import numpy as np
 import pandas as pd
 import scipy.stats as stats
 from scipy.cluster.hierarchy import linkage, fcluster, cophenet
-from scipy.spatial import distance
 from scipy.spatial.distance import pdist
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.decomposition import NMF
@@ -175,36 +174,36 @@ def get_states_from_h(h, n_states, max_std=3, n_clusterings=50, filename_prefix=
     return labels.iloc[:, :-1], labels.iloc[:, -1:], memberships
 
 
-def rank_features_against_reference(features, ref, features_type='continuous', ref_type='continuous',
-                                    features_ascending=False, ref_ascending=False, ref_sort=True,
-                                    metric='information_coef', n_features=0.95, n_samplings=30, confidence=0.95,
+def rank_features_against_reference(features, ref, feature_type='continuous', ref_type='continuous',
+                                    feature_ascending=False, ref_ascending=False, ref_sort=True,
+                                    function=information_coefficient, n_features=0.95, n_samplings=30, confidence=0.95,
                                     n_perms=30, title=None, title_size=16, annotation_label_size=9, plot_colname=False,
                                     result_filename=None, figure_filename=None, figure_size='auto', dpi=DPI):
     """
-    Compute features vs. `ref`.
-    :param features: pandas DataFrame (nfeatures, nelements), must have indices and columns
-    :param ref: pandas Series (nelements), must have name and columns, which must match 'features`'s
-    :param features_type: str, {continuous, categorical, binary}
-    :param ref_type: str, {continuous, categorical, binary}
-    :param features_ascending: bool, True if features score increase from top to bottom, False otherwise
-    :param ref_ascending: bool, True if ref values increase from left to right, False otherwise
-    :param ref_sort: bool, sort each ref or not
-    :param metric: str, {information_coef}
-    :param n_features: int or float, number threshold if >= 1 and quantile threshold if < 1
-    :param n_samplings: int, number of sampling for confidence interval bootstrapping; must be > 2 for CI computation
-    :param confidence: float, confidence interval
-    :param n_perms: int, number of permutations for permutation test
-    :param title: str, plot title
-    :param title_size: int, title text size
-    :param annotation_label_size: int, annotation text size
-    :param plot_colname: bool, plot column names or not
-    :param result_filename: str, file path to the output result
-    :param figure_filename: str, file path to the output figure
+    Compute 'features' vs. `ref`.
+    :param features: pandas DataFrame; (n_features, n_samples); must have indices and columns
+    :param ref: pandas Series; (n_samples); must have name and columns, which must match `features`'s
+    :param feature_type: str; {'continuous', 'categorical', 'binary'}
+    :param ref_type: str; {'continuous', 'categorical', 'binary'}
+    :param feature_ascending: bool; True if features score increase from top to bottom, and False otherwise
+    :param ref_ascending: bool; True if ref values increase from left to right, and False otherwise
+    :param ref_sort: bool; sort `ref` or not
+    :param function: function; function to score
+    :param n_features: int or float; number threshold if >= 1 and percentile threshold if < 1
+    :param n_samplings: int; number of sampling for confidence interval bootstrapping; must be > 2 to compute CI
+    :param confidence: float; confidence interval
+    :param n_perms: int; number of permutations for permutation test
+    :param title: str; plot title
+    :param title_size: int; title text size
+    :param annotation_label_size: int; annotation text size
+    :param plot_colname: bool; plot column names or not
+    :param result_filename: str; file path to the output result
+    :param figure_filename: str; file path to the output figure
     :param figure_size: 'auto' or tuple;
-    :param dpi: int;
+    :param dpi: int; dots per square inch of pixel in the output figure
     :return: None
     """
-    print_log('Computing features vs. {} using {} metric ...'.format(ref.name, metric))
+    print_log('Computing features vs. {} using {} ...'.format(ref.name, function))
 
     # Convert features into pandas DataFrame
     if isinstance(features, pd.Series):
@@ -212,14 +211,15 @@ def rank_features_against_reference(features, ref, features_type='continuous', r
 
     # Use intersecting columns
     col_intersection = set(features.columns) & set(ref.index)
-    if not col_intersection:
-        raise ValueError('features and ref have 0 intersecting columns, having {} and {} columns respectively'.format(
-            features.shape[1], ref.size))
-    else:
-        print_log('features and ref have {} intersecting columns, having {} and {} columns respectively ...'.format(
-            len(col_intersection), features.shape[1], ref.size))
+    if col_intersection:
+        print_log('features ({} cols) and ref ({} cols) have {} intersecting columns.'.format(features.shape[1],
+                                                                                              ref.size,
+                                                                                              len(col_intersection)))
         features = features.ix[:, col_intersection]
         ref = ref.ix[col_intersection]
+    else:
+        raise ValueError('features and ref have 0 intersecting columns, having {} and {} columns respectively'.format(
+            features.shape[1], ref.size))
 
     # Drop rows having all 0 values
     features = features.ix[(features != 0).any(axis=1)]
@@ -230,32 +230,32 @@ def rank_features_against_reference(features, ref, features_type='continuous', r
         features = features.reindex_axis(ref.index, axis=1)
 
     # Compute scores
-    scores = compute_against_reference(features, ref, metric=metric, nfeatures=n_features, ascending=features_ascending,
-                                       nsampling=n_samplings, confidence=confidence, nperm=n_perms)
+    scores = compute_against_reference(features, ref, function=function,
+                                       n_features=n_features, ascending=feature_ascending,
+                                       n_samplings=n_samplings, confidence=confidence, n_perms=n_perms)
     features = features.reindex(scores.index)
 
     if result_filename:
-        establish_path(os.path.split(result_filename)[0])
+        establish_path(result_filename)
         pd.merge(features, scores, left_index=True, right_index=True).to_csv(result_filename, sep='\t')
-        print_log('Saved the result as {}.'.format(result_filename))
 
     # Make annotations
     annotations = pd.DataFrame(index=features.index)
     for idx, s in features.iterrows():
         if '{} MoE'.format(confidence) in scores.columns:
-            annotations.ix[idx, 'IC(\u0394)'] = '{0:.3f}'.format(scores.ix[idx, metric]) \
+            annotations.ix[idx, 'IC(\u0394)'] = '{0:.3f}'.format(scores.ix[idx, function]) \
                                                 + '({0:.3f})'.format(scores.ix[idx, '{} MoE'.format(confidence)])
         else:
-            annotations.ix[idx, 'IC(\u0394)'] = '{0:.3f}(x.xxx)'.format(scores.ix[idx, metric])
+            annotations.ix[idx, 'IC(\u0394)'] = '{0:.3f}(x.xxx)'.format(scores.ix[idx, function])
 
     annotations['P-val'] = ['{0:.3f}'.format(x) for x in scores.ix[:, 'Global P-value']]
     annotations['FDR'] = ['{0:.3f}'.format(x) for x in scores.ix[:, 'FDR (BH)']]
 
     # Limit features to be plotted
     if n_features < 1:
-        above_quantile = scores.ix[:, metric] >= scores.ix[:, metric].quantile(n_features)
+        above_quantile = scores.ix[:, function] >= scores.ix[:, function].quantile(n_features)
         print_log('Plotting {} features vs. reference > {} quantile ...'.format(sum(above_quantile), n_features))
-        below_quantile = scores.ix[:, metric] <= scores.ix[:, metric].quantile(1 - n_features)
+        below_quantile = scores.ix[:, function] <= scores.ix[:, function].quantile(1 - n_features)
         print_log('Plotting {} features vs. reference < {} quantile ...'.format(sum(below_quantile), 1 - n_features))
         indices_to_plot = features.index[above_quantile | below_quantile].tolist()
     else:
@@ -263,66 +263,61 @@ def rank_features_against_reference(features, ref, features_type='continuous', r
         print_log('Plotting top & bottom {} features vs. reference ...'.format(len(indices_to_plot)))
 
     plot_features_against_reference(features.ix[indices_to_plot, :], ref, annotations.ix[indices_to_plot, :],
-                                    feature_type=features_type, ref_type=ref_type,
+                                    feature_type=feature_type, ref_type=ref_type,
                                     figure_size=figure_size, title=title, title_size=title_size,
                                     annotation_header=' ' * 7 + 'IC(\u0394)' + ' ' * 9 + 'P-val' + ' ' * 4 + 'FDR',
                                     annotation_label_size=annotation_label_size, plot_colname=plot_colname,
                                     output_filepath=figure_filename, dpi=dpi)
 
 
-def compute_against_reference(features, ref, metric='information_coef', nfeatures=0.95, ascending=False,
-                              nsampling=30, confidence=0.95, nperm=30):
+def compute_against_reference(features, ref, function=information_coefficient, n_features=0.95, ascending=False,
+                              n_samplings=30, confidence=0.95, n_perms=30):
     """
-    Compute scores[i] = `features`[i] vs. `ref` with computation using `metric` and get CI, p-val, and FDR (BH).
-    :param features: pandas DataFrame (nfeatures, nelements), must have indices and columns
-    :param ref: pandas Series (nelements), must have indices, which must match 'features`'s columns
-    :param metric: str, {information_coef}
-    :param nfeatures: int or float, number threshold if >= 1 and quantile threshold if < 1
-    :param ascending: bool, True if score increase from top to bottom, False otherwise
-    :param nsampling: int, number of sampling for confidence interval bootstrapping; must be > 2 for CI computation
-    :param confidence: float, confidence intrval
-    :param nperm: int, number of permutations for permutation test
+    Compute scores[i] = `features`[i] vs. `ref` using `metric` and get CI, p-val, and FDR (BH).
+    :param features: pandas DataFrame; (n_features, n_samples); must have indices and columns
+    :param ref: pandas Series; (n_samples); must have name and columns, which must match `features`'s
+    :param function: function; function to score
+    :param n_features: int or float; number threshold if >= 1 and percentile threshold if < 1
+    :param ascending: bool; True if score increase from top to bottom, and False otherwise
+    :param n_samplings: int; number of sampling for confidence interval bootstrapping; must be > 2 to compute CI
+    :param confidence: float; confidence interval
+    :param n_perms: int; number of permutations for permutation test
     :return: pandas DataFrame (nfeatures, nscores),
     """
     # Set computing function
-    if metric is 'information_coef':
-        function = information_coefficient
-    else:
-        raise ValueError('Unknown metric {}.'.format(metric))
-    print_log('Computing scores using {} metric ...'.format(metric))
-
+    print_log('Computing scores using {} metric ...'.format(function))
     # Compute and rank
     scores = np.empty(features.shape[0])
     for i, (idx, s) in enumerate(features.iterrows()):
         if i % 1000 is 0:
             print_log('\t{}/{} ...'.format(i, features.shape[0]))
         scores[i] = function(s, ref)
-    scores = pd.DataFrame(scores, index=features.index, columns=[metric]).sort_values(metric)
+    scores = pd.DataFrame(scores, index=features.index, columns=[function]).sort_values(function)
 
     print_log('Bootstrapping to get {} confidence interval ...'.format(confidence))
-    nsample = math.ceil(0.632 * features.shape[1])
-    if nsampling < 2:
+    n_samples = math.ceil(0.632 * features.shape[1])
+    if n_samplings < 2:
         print_log('Not bootstrapping because number of sampling < 3.')
-    elif nsample < 3:
+    elif n_samples < 3:
         print_log('Not bootstrapping because 0.632 * number of sample < 3.')
     else:
         # Limit features to be bootstrapped
-        if nfeatures < 1:  # limit using quantile
-            above_quantile = scores.ix[:, metric] >= scores.ix[:, metric].quantile(nfeatures)
-            print_log('Bootstrapping {} features vs. reference > {} quantile ...'.format(sum(above_quantile),
-                                                                                         nfeatures))
-            below_quantile = scores.ix[:, metric] <= scores.ix[:, metric].quantile(1 - nfeatures)
-            print_log('Bootstrapping {} features vs. reference < {} quantile ...'.format(sum(below_quantile),
-                                                                                         1 - nfeatures))
+        if n_features < 1:  # limit using percentile
+            above_quantile = scores.ix[:, function] >= scores.ix[:, function].quantile(n_features)
+            print_log('Bootstrapping {} features vs. reference > {} percentile ...'.format(sum(above_quantile),
+                                                                                           n_features))
+            below_quantile = scores.ix[:, function] <= scores.ix[:, function].quantile(1 - n_features)
+            print_log('Bootstrapping {} features vs. reference < {} percentile ...'.format(sum(below_quantile),
+                                                                                           1 - n_features))
             indices_to_bootstrap = scores.index[above_quantile | below_quantile].tolist()
         else:  # limit using numbers
-            indices_to_bootstrap = scores.index[:nfeatures].tolist() + scores.index[-nfeatures:].tolist()
+            indices_to_bootstrap = scores.index[:n_features].tolist() + scores.index[-n_features:].tolist()
             print_log('Bootstrapping top & bottom {} features vs. reference ...'.format(len(indices_to_bootstrap)))
 
         # Random sample columns and compute scores using the sampled columns
-        sampled_scores = pd.DataFrame(index=indices_to_bootstrap, columns=range(nsampling))
+        sampled_scores = pd.DataFrame(index=indices_to_bootstrap, columns=range(n_samplings))
         for c in sampled_scores:
-            sample_indices = np.random.choice(features.columns.tolist(), int(nsample)).tolist()
+            sample_indices = np.random.choice(features.columns.tolist(), int(n_samples)).tolist()
             sampled_features = features.ix[indices_to_bootstrap, sample_indices]
             sampled_ref = ref.ix[sample_indices]
             for idx, s in sampled_features.iterrows():
@@ -337,13 +332,12 @@ def compute_against_reference(features, ref, metric='information_coef', nfeature
             confidence_intervals.ix[i, 0] = moe
         scores = pd.merge(scores, confidence_intervals, how='outer', left_index=True, right_index='True')
 
-    print_log('Performing permutation test with {} permutations ...'.format(nperm))
-    permutation_pvals_and_fdrs = pd.DataFrame(index=features.index,
-                                              columns=['Local P-value', 'Global P-value', 'FDR (BH)'])
+    print_log('Performing permutation test with {} permutations ...'.format(n_perms))
+    permutation_pvals_and_fdrs = pd.DataFrame(index=features.index, columns=['Local P-value', 'Global P-value', 'FDR'])
     # Compute scores using permuted ref
-    permutation_scores = np.empty((features.shape[0], nperm))
+    permutation_scores = np.empty((features.shape[0], n_perms))
     shuffled_ref = np.array(ref)
-    for i in range(nperm):
+    for i in range(n_perms):
         np.random.shuffle(shuffled_ref)
         for j, (idx, s) in enumerate(features.iterrows()):
             permutation_scores[j, i] = function(s, shuffled_ref)
@@ -351,18 +345,18 @@ def compute_against_reference(features, ref, metric='information_coef', nfeature
     # Compute permutation P-values and FDRs
     all_permutation_scores = permutation_scores.flatten()
     for i, (idx, f) in enumerate(scores.iterrows()):
-        local_pval = float(sum(permutation_scores[i, :] > float(f.ix[metric])) / nperm)
+        local_pval = float(sum(permutation_scores[i, :] > float(f.ix[function])) / n_perms)
         if not local_pval:
-            local_pval = float(1 / nperm)
+            local_pval = float(1 / n_perms)
         permutation_pvals_and_fdrs.ix[idx, 'Local P-value'] = local_pval
 
-        global_pval = float(sum(all_permutation_scores > float(f.ix[metric])) / (nperm * features.shape[0]))
+        global_pval = float(sum(all_permutation_scores > float(f.ix[function])) / (n_perms * features.shape[0]))
         if not global_pval:
-            global_pval = float(1 / (nperm * features.shape[0]))
+            global_pval = float(1 / (n_perms * features.shape[0]))
         permutation_pvals_and_fdrs.ix[idx, 'Global P-value'] = global_pval
 
     permutation_pvals_and_fdrs.ix[:, 'FDR (BH)'] = multipletests(permutation_pvals_and_fdrs.ix[:, 'Global P-value'],
                                                                  method='fdr_bh')[1]
     scores = pd.merge(scores, permutation_pvals_and_fdrs, left_index=True, right_index=True)
 
-    return scores.sort_values(metric, ascending=ascending)
+    return scores.sort_values(function, ascending=ascending)
