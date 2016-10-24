a91ba6bb1254840d5fb6fb40ed2dc9c19136aad2
KwatME <kwat.medetgul.ernar@gmail.com>
2016-10-23 23:06:11 -0700

    Set argument words for normalizing testing_h.

--- ccal/support.py
+++ ccal/support.py
@@ -1,13 +1,9 @@
 """
 Computational Cancer Analysis Library
 
-Authors:
+Author:
     Huwate (Kwat) Yeerna (Medetgul-Ernar)
         kwat.medetgul.ernar@gmail.com
-        Computational Cancer Analysis Laboratory, UCSD Cancer Center
-
-    Pablo Tamayo
-        ptamayo@ucsd.edu
         Computational Cancer Analysis Laboratory, UCSD Cancer Center
 """
 
