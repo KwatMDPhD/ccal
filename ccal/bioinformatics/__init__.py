f3b6e0ec04fda41aa2ffbe1c365fa7fd302cb508
KwatME <kwat.medetgul.ernar@gmail.com>
2016-12-17 15:49:34 -0800

    VERSOBE is False by default

--- ccal/bioinformatics/__init__.py
+++ ccal/bioinformatics/__init__.py
@@ -1,3 +1,16 @@
+"""
+Computational Cancer Analysis Library
+
+Authors:
+    Huwate (Kwat) Yeerna (Medetgul-Ernar)
+        kwat.medetgul.ernar@gmail.com
+        Computational Cancer Analysis Laboratory, UCSD Cancer Center
+
+    Pablo Tamayo
+        ptamayo@ucsd.edu
+        Computational Cancer Analysis Laboratory, UCSD Cancer Center
+"""
+
 from os import environ
 from os.path import join, isfile, isdir
 
