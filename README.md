a17fff8dd4e395dca3c2759545d2467d33f9544c
KwatME <kwat.medetgul.ernar@gmail.com>
2017-05-05 14:43:52 -0700

    Breaking this repo up

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-![Alt text](media/ccal_logo.png?raw=true "Optional Title")
-
-Authors
--
-#### Huwate (Kwat) Yeerna (Medetgul-Ernar)
-kwat.medetgul.ernar@gmail.com
-
-Computational Cancer Analysis Laboratory, UCSD Cancer Center
-
-#### Pablo Tamayo
-ptamayo@ucsd.edu
-
-Computational Cancer Analysis Laboratory, UCSD Cancer Center
+![](media/ccal_logo.png?raw=true) Library for hunting cancers :crab::gun:
--- ccal/__init__.py
+++ ccal/__init__.py
@@ -4,23 +4,11 @@ Computational Cancer Analysis Library
 Authors:
     Huwate (Kwat) Yeerna (Medetgul-Ernar)
         kwat.medetgul.ernar@gmail.com
-        Computational Cancer Analysis Laboratory, UCSD Cancer Center
+        Computational Cancer Analysis Laboratory, UCSD Moore's Cancer Center
 
     Pablo Tamayo
         ptamayo@ucsd.edu
-        Computational Cancer Analysis Laboratory, UCSD Cancer Center
+        Computational Cancer Analysis Laboratory, UCSD Moore's Cancer Center
 """
 
-import sys
-
 RANDOM_SEED = 20121020
-
-from .computational_biology import (association, gsea, inference,
-                                    mutational_signature, mutual_vulnerability,
-                                    oncogps)
-from .helper.file import (load_data_table, read_gct, read_gmt, read_gmts,
-                          write_data_table, write_gct, write_rnk)
-from .helper.plot import (plot_clustermap, plot_distribution, plot_heatmap,
-                          plot_nmf, plot_points, plot_violin_box_or_bar)
-
-sys.setrecursionlimit(10000)
