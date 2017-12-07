50a5a1cffbdb093b6f13a950b498214a0cdcf872
KwatME <kwatme8@gmail.com>
2017-12-06 23:26:02 -0800

    0

--- ccal/__init__.py
+++ ccal/__init__.py
@@ -1,90 +1,66 @@
-from .cluster.cluster.count_coclustering_and_normalize import \
-    count_coclustering_and_normalize
-from .cluster.cluster.hierarchical_cluster_distance_and_compute_ccc import \
-    hierarchical_cluster_distance_and_compute_ccc
-from .cluster.cluster.hierarchical_consensus_cluster import \
-    hierarchical_consensus_cluster
-from .cluster.cluster.hierarchical_consensus_cluster_with_multiple_k import \
-    hierarchical_consensus_cluster_with_multiple_k
 from .cluster.cluster.nmf_consensus_cluster import nmf_consensus_cluster
-from .cluster.cluster.nmf_consensus_cluster_with_multiple_k import \
-    nmf_consensus_cluster_with_multiple_k
-from .file.file.access_gct import read_gct, write_gct
-from .file.file.access_gmt import read_gmts, write_gmt
-from .geo.geo.get_and_parse_geo_data import get_and_parse_geo_data
-from .information.information.compute_brier_entropy import \
-    compute_brier_entropy
-from .information.information.compute_entropy import compute_entropy
-from .information.information.compute_information_coefficient import (compute_information_coefficient,
-                                                                      compute_information_distance)
-from .information.information.normalize_information_coefficient import \
-    normalize_information_coefficients
-from .match.match.make_comparison_panel import make_comparison_panel
-from .match.match.make_match_panel import make_match_panel
+from .cluster.cluster.count_coclustering_and_normalize import count_coclustering_and_normalize
+from .cluster.cluster.hierarchical_cluster_distance_and_compute_ccc import hierarchical_cluster_distance_and_compute_ccc
+from .cluster.cluster.hierarchical_consensus_cluster_with_multiple_k import hierarchical_consensus_cluster_with_multiple_k
+from .cluster.cluster.nmf_consensus_cluster_with_multiple_k import nmf_consensus_cluster_with_multiple_k
+from .cluster.cluster.hierarchical_consensus_cluster import hierarchical_consensus_cluster
 from .match.match.make_summary_match_panel import make_summary_match_panel
-from .match.match.match import (match,
-                                match_randomly_sampled_target_and_features_to_compute_margin_of_errors,
-                                match_target_and_features,
-                                permute_target_and_match_target_and_features)
+from .match.match.make_comparison_panel import make_comparison_panel
 from .match.match.plot_match_panel import plot_match_panel
+from .match.match.match import match
+from .match.match.match import match_randomly_sampled_target_and_features_to_compute_margin_of_errors
+from .match.match.match import permute_target_and_match_target_and_features
+from .match.match.match import match_target_and_features
+from .match.match.make_match_panel import make_match_panel
 from .matrix_decomposition.matrix_decomposition.nmf import nmf
-from .matrix_decomposition.matrix_decomposition.solve_for_nmf_h import \
-    solve_for_nmf_h
-from .nd_array.nd_array.apply_function_on_2_1d_arrays_and_compute_empirical_p_value import \
-    apply_function_on_2_1d_arrays_and_compute_empirical_p_value
-from .nd_array.nd_array.apply_function_on_2_2d_arrays_slices import \
-    apply_function_on_2_2d_arrays_slices
-from .nd_array.nd_array.cluster_2d_array_rows import cluster_2d_array_rows
-from .nd_array.nd_array.cluster_2d_array_slices_by_group import \
-    cluster_2d_array_slices_by_group
-from .nd_array.nd_array.compute_empirical_p_value import \
-    compute_empirical_p_value
-from .nd_array.nd_array.compute_empirical_p_values_and_fdrs import \
-    compute_empirical_p_values_and_fdrs
-from .nd_array.nd_array.compute_log2_ratios import compute_log2_ratios
-from .nd_array.nd_array.compute_margin_of_error import compute_margin_of_error
-from .nd_array.nd_array.define_exponential_function import \
-    define_exponential_function
-from .nd_array.nd_array.drop_nan_and_apply_function_on_2_1d_arrays import \
-    drop_nan_and_apply_function_on_2_1d_arrays
-from .nd_array.nd_array.fit_function_on_each_2d_array_slice import \
-    fit_function_on_each_2d_array_slice
-from .nd_array.nd_array.get_1d_array_unique_objects_in_order import \
-    get_1d_array_unique_objects_in_order
-from .nd_array.nd_array.make_index_and_fraction_grid_coordinates_pair import \
-    make_index_and_fraction_grid_coordinates_pair
-from .nd_array.nd_array.make_nd_grid_coordinates import \
-    make_nd_grid_coordinates
-from .nd_array.nd_array.normalize_1d_array import normalize_1d_array
-from .nd_array.nd_array.normalize_1d_array_mean_to_be_0_and_clip import \
-    normalize_1d_array_mean_to_be_0_and_clip
-from .nd_array.nd_array.normalize_2d_array import normalize_2d_array
-from .nd_array.nd_array.shuffle_each_2d_array_slice import \
-    shuffle_each_2d_array_slice
-from .onco_gps.onco_gps.GPSMap import GPSMap
-from .onco_gps.onco_gps.make_grid_values_and_categorical_phenotypes import \
-    make_grid_values_and_categorical_phenotypes
-from .onco_gps.onco_gps.make_grid_values_and_continuous_phenotypes import \
-    make_grid_values_and_continuous_phenotypes
-from .onco_gps.onco_gps.make_node_x_dimension import make_node_x_dimension
-from .onco_gps.onco_gps.make_sample_x_dimension import make_sample_x_dimension
-from .plot.plot.assign_colors import assign_colors
-from .plot.plot.decorate import decorate
-from .plot.plot.get_ax_positions_relative_to_ax import \
-    get_ax_positions_relative_to_ax
-from .plot.plot.get_ax_positions_relative_to_figure import \
-    get_ax_positions_relative_to_figure
-from .plot.plot.make_categorical_colormap import make_categorical_colormap
-from .plot.plot.make_random_categorical_colormap import \
-    make_random_categorical_colormap
-from .plot.plot.make_random_color import make_random_color
-from .plot.plot.plot_clustermap import plot_clustermap
-from .plot.plot.plot_columns import plot_columns
-from .plot.plot.plot_distribution import plot_distribution
-from .plot.plot.plot_heatmap import plot_heatmap
-from .plot.plot.plot_lines import plot_lines
+from .matrix_decomposition.matrix_decomposition.solve_for_nmf_h import solve_for_nmf_h
+from .geo.geo.get_and_parse_geo_data import get_and_parse_geo_data
+from .file.file.access_gmt import read_gmts
+from .file.file.access_gmt import write_gmt
+from .file.file.access_gct import read_gct
+from .file.file.access_gct import write_gct
+from .information.information.compute_entropy import compute_entropy
+from .information.information.compute_information_coefficient import compute_information_distance
+from .information.information.compute_information_coefficient import compute_information_coefficient
+from .information.information.normalize_information_coefficient import normalize_information_coefficients
+from .information.information.compute_brier_entropy import compute_brier_entropy
 from .plot.plot.plot_nmf import plot_nmf
+from .plot.plot.save_plot import save_plot
 from .plot.plot.plot_points import plot_points
+from .plot.plot.make_random_color import make_random_color
+from .plot.plot.assign_colors import assign_colors
+from .plot.plot.make_random_categorical_colormap import make_random_categorical_colormap
+from .plot.plot.plot_heatmap import plot_heatmap
+from .plot.plot.get_ax_positions_relative_to_figure import get_ax_positions_relative_to_figure
+from .plot.plot.plot_clustermap import plot_clustermap
 from .plot.plot.plot_samples import plot_samples
 from .plot.plot.plot_violin_box_or_bar import plot_violin_box_or_bar
-from .plot.plot.save_plot import save_plot
+from .plot.plot.plot_lines import plot_lines
+from .plot.plot.plot_columns import plot_columns
+from .plot.plot.get_ax_positions_relative_to_ax import get_ax_positions_relative_to_ax
+from .plot.plot.make_categorical_colormap import make_categorical_colormap
+from .plot.plot.decorate import decorate
+from .plot.plot.plot_distribution import plot_distribution
+from .nd_array.nd_array.make_index_and_fraction_grid_coordinates_pair import make_index_and_fraction_grid_coordinates_pair
+from .nd_array.nd_array.drop_nan_and_apply_function_on_2_1d_arrays import drop_nan_and_apply_function_on_2_1d_arrays
+from .nd_array.nd_array.make_nd_grid_coordinates import make_nd_grid_coordinates
+from .nd_array.nd_array.shuffle_each_2d_array_slice import shuffle_each_2d_array_slice
+from .nd_array.nd_array.normalize_2d_array import normalize_2d_array
+from .nd_array.nd_array.define_exponential_function import define_exponential_function
+from .nd_array.nd_array.compute_empirical_p_values_and_fdrs import compute_empirical_p_values_and_fdrs
+from .nd_array.nd_array.compute_margin_of_error import compute_margin_of_error
+from .nd_array.nd_array.apply_function_on_2_2d_arrays_slices import apply_function_on_2_2d_arrays_slices
+from .nd_array.nd_array.normalize_1d_array import normalize_1d_array
+from .nd_array.nd_array.normalize_1d_array_mean_to_be_0_and_clip import normalize_1d_array_mean_to_be_0_and_clip
+from .nd_array.nd_array.fit_function_on_each_2d_array_slice import fit_function_on_each_2d_array_slice
+from .nd_array.nd_array.get_1d_array_unique_objects_in_order import get_1d_array_unique_objects_in_order
+from .nd_array.nd_array.apply_function_on_2_1d_arrays_and_compute_empirical_p_value import apply_function_on_2_1d_arrays_and_compute_empirical_p_value
+from .nd_array.nd_array.compute_log2_ratios import compute_log2_ratios
+from .nd_array.nd_array.cluster_2d_array_slices_by_group import cluster_2d_array_slices_by_group
+from .nd_array.nd_array.compute_empirical_p_value import compute_empirical_p_value
+from .nd_array.nd_array.cluster_2d_array_rows import cluster_2d_array_rows
+from .onco_gps.onco_gps.make_node_x_dimension import make_node_x_dimension
+from .onco_gps.onco_gps.make_grid_values_and_continuous_phenotypes import make_grid_values_and_continuous_phenotypes
+from .onco_gps.onco_gps.make_sample_x_dimension import make_sample_x_dimension
+from .onco_gps.onco_gps.make_grid_values_and_categorical_phenotypes import make_grid_values_and_categorical_phenotypes
+from .onco_gps.onco_gps.GPSMap import GPSMap
