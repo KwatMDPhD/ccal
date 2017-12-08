5288b5753767c85c21fc28b778f5c24755570f73
KwatME <kwatme8@gmail.com>
2017-12-08 14:16:53 -0800

    Added GSEA: 0.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ for dp, dns, fns in walk(NAME):
 
 setup(
     name=NAME,
-    version='0.2.0',
+    version='0.2.1',
     description=
     'Computational Cancer Analysis Library: bioinformatics library for hunting cancers',
     long_description='See {} to learn more.'.format(URL),
