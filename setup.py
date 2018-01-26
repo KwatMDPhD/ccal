1650f9011915047c9b9c2cbc2c4366f894c5de71
KwatME <kwatme8@gmail.com>
2018-01-25 17:11:42 -0800

    0.3.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ for dp, dns, fns in walk(NAME):
 
 setup(
     name=NAME,
-    version='0.2.7',
+    version='0.3.0',
     description=
     'Computational Cancer Analysis Library: bioinformatics library for hunting cancers',
     long_description='See {} to learn more.'.format(URL),
