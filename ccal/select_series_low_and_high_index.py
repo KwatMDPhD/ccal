fae6faaf0bd45b80cfbf0029317eb812db3e0ff7
KwatME <kwatme8@gmail.com>
2018-11-24 00:40:25 -0800

    Git sync (0)

---
+++ ccal/select_series_low_and_high_index.py
@@ -0,0 +1,80 @@
+from ccal import plot_points
+from pandas import Series
+
+
+def select_series_low_and_high_index(
+        values,
+        low_margin_factor=1,
+        high_margin_factor=1,
+        title=None,
+        file_path_prefix=None,
+):
+
+    values = values.sort_values()
+
+    margin = values.std() / 2
+
+    low_index = values.index[
+        values < values.mean() - margin * low_margin_factor]
+
+    high_index = values.index[
+        values.mean() + margin * high_margin_factor < values]
+
+    rank = Series(
+        range(values.size),
+        index=values.index,
+    )
+
+    if file_path_prefix is not None:
+
+        Series(
+            low_index,
+            name='Low Index',
+        ).to_csv(
+            '{}.low.tsv'.format(file_path_prefix),
+            sep='\t',
+        )
+
+        Series(
+            high_index,
+            name='High Index',
+        ).to_csv(
+            '{}.high.tsv'.format(file_path_prefix),
+            sep='\t',
+        )
+
+        html_file_path = '{}.low_and_high.html'.format(file_path_prefix)
+
+    else:
+
+        html_file_path = None
+
+    plot_points(
+        (
+            rank,
+            rank[high_index],
+            rank[low_index],
+        ),
+        (
+            values,
+            values[high_index],
+            values[low_index],
+        ),
+        names=(
+            'All',
+            'High',
+            'Low',
+        ),
+        modes=('lines', ) * 3,
+        texts=(
+            values.index,
+            high_index,
+            low_index,
+        ),
+        title=title,
+        xaxis_title='Rank',
+        yaxis_title='Value',
+        html_file_path=html_file_path,
+    )
+
+    return low_index, high_index
