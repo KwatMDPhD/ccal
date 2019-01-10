03a75a80a0d5023b4b2f984175d36359f9c0ec49
KwatME <kwatme8@gmail.com>
2019-01-09 22:56:32 -0800

    ccal

--- setup.py
+++ setup.py
@@ -4,53 +4,20 @@ from setuptools import setup
 
 from ccal import VERSION
 
-name = "ccal"
-
-url = "https://github.com/KwatME/ccal"
-
-strs_to_skip = (".git", "__pycache__")
-
-packages = []
-
-for directory_path, directory_names, file_names in walk(name):
-
-    if not any(str_ in directory_path for str_ in strs_to_skip):
-
-        if "/data/" not in directory_path:
-
-            packages.append(directory_path)
-
 package_data = []
 
-for directory_path, directory_names, file_names in walk(
-    "{}/sequencing_process/data".format(name)
-):
+for directory_path, directory_names, file_names in walk("data"):
 
-    if not any(str_ in directory_path for str_ in strs_to_skip):
+    for file_name in file_names:
 
-        for file_name in file_names:
-
-            package_data.append(
-                "{}/{}".format(directory_path.split(sep="/", maxsplit=1)[1], file_name)
-            )
-
-for directory_path, directory_names, file_names in walk("{}/gene/data".format(name)):
-
-    if not any(str_ in directory_path for str_ in strs_to_skip):
-
-        for file_name in file_names:
-
-            package_data.append(
-                "{}/{}".format(directory_path.split(sep="/", maxsplit=1)[1], file_name)
-            )
+        package_data.append("{}/{}".format(directory_path, file_name))
 
 setup(
-    name=name,
+    name="ccal",
     version=VERSION,
     description="Computational Cancer Analysis Library",
-    long_description="See {} to learn more.".format(url),
-    url=url,
-    author="(Kwat) Huwate Yeerna",
+    url="https://github.com/KwatME/ccal",
+    author="Kwat Medetgul-Ernar (Huwate Yeerna)",
     author_email="kwatme8@gmail.com",
     license="LICENSE",
     classifiers=(
@@ -61,9 +28,8 @@ setup(
         "Natural Language :: English",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ),
-    packages=packages,
     python_requires=">=3.6",
     install_requires=(),
-    package_data={"ccal": package_data},
     include_package_data=True,
+    package_data={"ccal": package_data},
 )
