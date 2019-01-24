42550be799ddd093add44f14be14371e1205ddef
KwatME <kwatme8@gmail.com>
2019-01-23 22:17:04 -0800

    if else

--- ccal/select_tcga_sample_by_sample_type_and_group.py
+++ ccal/select_tcga_sample_by_sample_type_and_group.py
@@ -21,14 +21,20 @@ def select_tcga_sample_by_sample_type_and_group(df, sample_type="01"):
 
     print(df_duplicated.shape)
 
-    df_duplicated_grouped = df_duplicated.groupby(
-        by=df_duplicated.columns.str[:12], axis=1
-    ).mean()
+    if df_duplicated.empty:
 
-    print(df_duplicated_grouped.shape)
+        df = df_not_duplicated
 
-    df = concat((df_not_duplicated, df_duplicated_grouped), axis=1)
+    else:
 
-    print(df.shape)
+        df_duplicated_grouped = df_duplicated.groupby(
+            by=df_duplicated.columns.str[:12], axis=1
+        ).mean()
+
+        print(df_duplicated_grouped.shape)
+
+        df = concat((df_not_duplicated, df_duplicated_grouped), axis=1)
+
+        print(df.shape)
 
     return df
