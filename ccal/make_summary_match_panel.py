e08a8d960036d6efe6c68c5a85dc926848ec2341
KwatME <kwatme8@gmail.com>
2019-02-01 17:36:17 -0800

    _0

--- ccal/make_summary_match_panel.py
+++ ccal/make_summary_match_panel.py
@@ -2,7 +2,7 @@ from ._make_annotations import _make_annotations
 from ._process_target_or_data_for_plotting import _process_target_or_data_for_plotting
 from .plot_and_save import plot_and_save
 
-eps = 1e-8
+_0 = 1e-8
 
 
 def make_summary_match_panel(
@@ -61,7 +61,7 @@ def make_summary_match_panel(
 
     domain_start = domain_end - row_fraction
 
-    if abs(domain_start) <= eps:
+    if abs(domain_start) <= _0:
 
         domain_start = 0
 
@@ -112,13 +112,13 @@ def make_summary_match_panel(
 
         domain_end = domain_start - row_fraction
 
-        if abs(domain_end) <= eps:
+        if abs(domain_end) <= _0:
 
             domain_end = 0
 
         domain_start = domain_end - data_dict["df"].shape[0] * row_fraction
 
-        if abs(domain_start) <= eps:
+        if abs(domain_start) <= _0:
 
             domain_start = 0
 
