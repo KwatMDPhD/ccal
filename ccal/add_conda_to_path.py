b37686d4d232775e4d6f15a842fc2901ef4ccb17
KwatME <kwatme8@gmail.com>
2019-03-26 15:59:01 -0700

    REFACTOR

--- ccal/add_conda_to_path.py
+++
@@ -1,8 +0,0 @@
-from os import environ
-
-
-def add_conda_to_path(conda_directory_path):
-
-    environ["PATH"] = "{}:{}".format(
-        "{}/{}".format(conda_directory_path, "bin"), environ["PATH"]
-    )
--- ccal/shutdown_machine.py
+++
@@ -1,6 +0,0 @@
-from .run_command import run_command
-
-
-def shutdown_machine():
-
-    run_command("sudo shutdown -h now")
