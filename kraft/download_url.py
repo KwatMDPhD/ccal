9b7ac4a18dfa8966de6e04e51f7aed247a2e180f
KwatME <kwatme8@gmail.com>
2019-05-09 19:26:24 -0700

    Refactoring

--- kraft/download_url.py
+++ kraft/download_url.py
@@ -8,6 +8,8 @@ def download_url(url, directory_path):
 
     file_path = join(directory_path, basename(urlsplit(url).path))
 
+    print(f"{url} =(download)=> {file_path} ...")
+
     with open(file_path, "wb") as file:
 
         file.write(get(url, allow_redirects=True).content)
