97fce1718d8d5c7082c9394351dd7d591d6390aa
KwatME <kwatme8@gmail.com>
2019-05-18 21:54:38 -0700

    save

--- kraft/get_variants_from_vcf_or_vcf_gz.py
+++ kraft/get_variants_from_vcf_or_vcf_gz.py
@@ -15,11 +15,11 @@ def get_variants_from_vcf_or_vcf_gz(
         run_command(f"tabix {vcf_or_vcf_gz_file_path}")
 
     return tuple(
-        tuple(vcf_row.split("\t"))
+        tuple(vcf_row.split(sep="\t"))
         for vcf_row in run_command(
             f"tabix {vcf_or_vcf_gz_file_path} {chromosome}:{_1_indexed_inclusive_start_position}-{_1_indexed_inclusive_end_position}"
         )
         .stdout.strip()
-        .split("\n")
+        .split(sep="\n")
         if vcf_row != ""
     )
