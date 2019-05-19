4a09fc635975a629925365f3cb3768bce2625bf7
KwatME <kwatme8@gmail.com>
2019-05-19 16:45:04 -0700

    Refactored

---
+++ kraft/print_header_in_terminal.py
@@ -0,0 +1,20 @@
+from click import get_terminal_size
+
+from kraft import echo_or_print_str
+
+
+def print_header_in_terminal(str_):
+
+    terminal_width, terminal_height = get_terminal_size()
+
+    spaces = " " * max(0, (terminal_width - len(str_)) // 2)
+
+    str_ = spaces + str_ + spaces
+
+    if not terminal_width % 2:
+
+        str_ += " "
+
+    spacer = "=" * terminal_width
+
+    echo_or_print_str(f"{spacer}\n{str_}\n{spacer}", fg="random")
