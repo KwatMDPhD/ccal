98a9879dc1be0ec4f97c6e85ff3216dae0545314
KwatME <kwatme8@gmail.com>
2019-05-29 17:08:52 -0700

    Target dimension ...

--- kraft/infer.py
+++ kraft/infer.py
@@ -1,78 +1,62 @@
-from numpy import absolute, apply_along_axis, argmax, linspace, asarray
+from numpy import absolute, unique
 
-from .plot_mesh_grid import plot_mesh_grid
 from .compute_posterior_probability import compute_posterior_probability
-from .make_mesh_grid_point_x_dimension import make_mesh_grid_point_x_dimension
-from .unmesh import unmesh
+from .plot_mesh_grid import plot_mesh_grid
 
 
-def _get_target_mesh_grid_point_x_dimension(nd_array, function):
-
-    return tuple(
-        make_mesh_grid_point_x_dimension(
-            (
-                linspace(
-                    0,
-                    nd_array.shape[i],
-                    num=nd_array.shape[i],
-                    endpoint=False,
-                    dtype=int,
-                )
-                for i in range(nd_array.ndim - 1)
-            )
-        ).T
-    ) + (apply_along_axis(function, -1, nd_array).ravel(),)
-
-
-def infer(observation_x_dimension, target="max", plot=True, dimension_names=None):
+def infer(
+    observation_x_dimension,
+    target_dimension_value,
+    dimension_fraction_grid_extensions=None,
+    dimension_n_grids=None,
+    plot=True,
+    dimension_names=None,
+):
 
     n_dimension = observation_x_dimension.shape[1]
 
-    mesh_grid_point_x_dimensiona, mesh_grid_point_posterior_probability = compute_posterior_probability(
-        observation_x_dimension, plot=plot, dimension_names=dimension_names
+    mesh_grid_point_x_dimension, mesh_grid_point_posterior_probability = compute_posterior_probability(
+        observation_x_dimension,
+        dimension_fraction_grid_extensions=dimension_fraction_grid_extensions,
+        dimension_n_grids=dimension_n_grids,
+        plot=plot,
+        dimension_names=dimension_names,
     )
 
-    dimension_grids, posterior_probability = unmesh(
-        mesh_grid_point_x_dimensiona, mesh_grid_point_posterior_probability
-    )
+    target_dimension_grid = unique(mesh_grid_point_x_dimension[:, -1])
 
-    if target == "max":
+    target_dimension_value_target_dimension_grid_index = absolute(
+        target_dimension_grid - target_dimension_value
+    ).argmin()
 
-        target_mesh_grid_point_x_dimension = _get_target_mesh_grid_point_x_dimension(
-            posterior_probability, argmax
-        )
+    mesh_grid_point_x_dimension_ = mesh_grid_point_x_dimension[
+        target_dimension_value_target_dimension_grid_index :: target_dimension_grid.size,
+        :-1,
+    ]
 
-    else:
-
-        i = absolute(dimension_grids[-1] - target).argmin()
-
-        target_mesh_grid_point_x_dimension = _get_target_mesh_grid_point_x_dimension(
-            posterior_probability, lambda _: i
-        )
-
-    mesh_grid_point_YYY = posterior_probability[target_mesh_grid_point_x_dimension]
-
-    mesh_grid_point_x_XXX = make_mesh_grid_point_x_dimension(dimension_grids[:-1])
+    mesh_grid_point_posterior_probability_ = mesh_grid_point_posterior_probability[
+        target_dimension_value_target_dimension_grid_index :: target_dimension_grid.size
+    ]
 
     if plot:
 
         if n_dimension == 2:
 
-            title = f"P({dimension_names[1]} = {target} | {dimension_names[0]})"
+            title = f"P({dimension_names[1]} = {target_dimension_value} | {dimension_names[0]})"
 
         elif n_dimension == 3:
 
-            title = f"P({dimension_names[2]} = {target} | {dimension_names[0]}, {dimension_names[1]})"
+            title = f"P({dimension_names[2]} = {target_dimension_value} | {dimension_names[0]}, {dimension_names[1]})"
 
         else:
 
             title = None
 
         plot_mesh_grid(
-            mesh_grid_point_x_XXX,
-            mesh_grid_point_YYY,
+            mesh_grid_point_x_dimension_,
+            mesh_grid_point_posterior_probability_,
             title=title,
             dimension_names=dimension_names,
         )
 
-    return posterior_probability  # , p_tvt__ntvs
+    return mesh_grid_point_x_dimension_, mesh_grid_point_posterior_probability_
--- kraft/infer_assuming_independence.py
+++ kraft/infer_assuming_independence.py
@@ -1,107 +1,115 @@
-from numpy import absolute, full, linspace, nan, product
-from pandas import DataFrame
-
-from .estimate_kernel_density import estimate_kernel_density
-from .infer import infer
-from .make_mesh_grid_point_x_dimension import make_mesh_grid_point_x_dimension
-from .N_GRID import N_GRID
-from .plot_and_save import plot_and_save
-from .plot_heat_map import plot_heat_map
-from .unmesh import unmesh
+from .compute_joint_probability import compute_joint_probability
+from numpy import absolute
 
 
 def infer_assuming_independence(
-    observation_x_dimension, n_grid=N_GRID, target="max", plot=True, names=None
+    observation_x_dimension,
+    target_dimension_value,
+    dimension_fraction_grid_extensions=None,
+    dimension_n_grids=None,
+    plot=True,
+    dimension_names=None,
 ):
 
     n_dimension = observation_x_dimension.shape[1]
 
-    n_ntv = n_dimension - 1
-
-    kd_tv = unmesh(
-        estimate_kernel_density(observation_x_dimension[:, -1].reshape(-1, 1)),
-        n_grid=n_grid,
-    )[1]
-
-    p_tv = kd_tv / kd_tv.sum()
-
-    grid_tv = linspace(
-        observation_x_dimension[:, -1].min(),
-        observation_x_dimension[:, -1].max(),
-        num=n_grid,
+    mesh_grid_point_x_dimension, mesh_grid_point_joint_probability = compute_joint_probability(
+        observation_x_dimension[:, -1:],
+        dimension_fraction_grid_extensions=dimension_fraction_grid_extensions[-1:],
+        dimension_n_grids=dimension_n_grids[-1:],
+        plot=plot,
+        dimension_names=dimension_names[-1:],
     )
 
-    if target == "max":
+    print(mesh_grid_point_x_dimension.shape, mesh_grid_point_joint_probability.shape)
 
-        t_i = p_tv.argmax()
+    target_dimension_value_target_dimension_grid_index = absolute(
+        mesh_grid_point_x_dimension - target_dimension_value
+    ).argmin()
 
-    else:
+    print(target_dimension_value_target_dimension_grid_index)
 
-        t_i = absolute(grid_tv - target).argmin()
-
-    t = grid_tv[t_i]
-
-    p_tvt = p_tv[t_i]
-
-    if names is None:
-
-        names = tuple(f"Variable {i}" for i in range(n_dimension))
-
-    if plot:
-
-        plot_and_save(
-            {
-                "layout": {
-                    "title": {"text": f"P({names[-1]} = {target} = {t}) = {p_tvt}"},
-                    "xaxis": {"title": names[-1]},
-                    "yaxis": {"title": "Probability"},
-                },
-                "data": [
-                    {
-                        "type": "scatter",
-                        "x": grid_tv,
-                        "y": p_tv,
-                        "marker": {"color": "#20d9ba"},
-                    }
-                ],
-            },
-            None,
-        )
-
-    p_tvt__1ntvs = tuple(
-        infer(
-            (variables[ntv_i], variables[-1]),
-            n_grid=n_grid,
-            target=t,
-            plot=False,
-            names=(names[ntv_i], names[-1]),
-        )[1]
-        for ntv_i in range(n_ntv)
-    )
-
-    p_tvt__ntvs = full((n_grid,) * n_ntv, nan)
-
-    mesh_grid_point_x_dimension = make_mesh_grid_point_x_dimension(
-        linspace(0, n_grid, num=n_grid, endpoint=False, dtype=int) for i in range(n_ntv)
-    )
-
-    for i in range(n_grid ** n_ntv):
-
-        indices = tuple(
-            [mesh_grid_point_x_dimension[i][ntv_i]] for ntv_i in range(n_ntv)
-        )
-
-        p_tvt__ntvs[indices] = product(
-            [p_tvt__1ntvs[j][indices[j]] for j in range(n_ntv)]
-        ) / p_tvt ** (n_ntv - 1)
-
-    if plot and n_dimension == 3:
-
-        plot_heat_map(
-            DataFrame(p_tvt__ntvs),
-            title=f"P({names[2]} = {target} = {t} | {names[0]}, {names[1]})",
-            xaxis_title=names[1],
-            yaxis_title=names[0],
-        )
-
-    return None, p_tvt__ntvs
+    # p_tv = kd_tv / kd_tv.sum()
+    #
+    # grid_tv = linspace(variables[-1].min(), variables[-1].max(), num=n_grid)
+    #
+    # if target == "max":
+    #
+    #     t_i = p_tv.argmax()
+    #
+    # else:
+    #
+    #     t_i = absolute(grid_tv - target).argmin()
+    #
+    # t = grid_tv[t_i]
+    #
+    # p_tvt = p_tv[t_i]
+    #
+    # if names is None:
+    #
+    #     names = tuple("Variable {}".format(i) for i in range(n_dimension))
+    #
+    # if plot:
+    #
+    #     plot_and_save(
+    #         {
+    #             "layout": {
+    #                 "title": {
+    #                     "text": "P({} = {} = {}) = {}".format(
+    #                         names[-1], target, t, p_tvt
+    #                     )
+    #                 },
+    #                 "xaxis": {"title": names[-1]},
+    #                 "yaxis": {"title": "Probability"},
+    #             },
+    #             "data": [
+    #                 {
+    #                     "type": "scatter",
+    #                     "x": grid_tv,
+    #                     "y": p_tv,
+    #                     "marker": {"color": "#20d9ba"},
+    #                 }
+    #             ],
+    #         },
+    #         None,
+    #     )
+    #
+    # p_tvt__1ntvs = tuple(
+    #     infer(
+    #         (variables[ntv_i], variables[-1]),
+    #         n_grid=n_grid,
+    #         target=t,
+    #         plot=False,
+    #         names=(names[ntv_i], names[-1]),
+    #     )[1]
+    #     for ntv_i in range(n_ntv)
+    # )
+    #
+    # p_tvt__ntvs = full((n_grid,) * n_ntv, nan)
+    #
+    # mesh_index_grid_raveled = make_mesh_grid_and_ravel(
+    #     (0,) * n_ntv, (n_grid - 1,) * n_ntv, (n_grid,) * n_ntv
+    # )
+    #
+    # for i in range(n_grid ** n_ntv):
+    #
+    #     indices = tuple(
+    #         [mesh_index_grid_raveled[ntv_i][i].astype(int)] for ntv_i in range(n_ntv)
+    #     )
+    #
+    #     p_tvt__ntvs[indices] = product(
+    #         [p_tvt__1ntvs[j][indices[j]] for j in range(n_ntv)]
+    #     ) / p_tvt ** (n_ntv - 1)
+    #
+    # if plot and n_dimension == 3:
+    #
+    #     plot_heat_map(
+    #         DataFrame(rot90(p_tvt__ntvs)),
+    #         title="P({} = {} = {} | {}, {})".format(
+    #             names[-1], target, t, names[0], names[1]
+    #         ),
+    #         xaxis_title=names[0],
+    #         yaxis_title=names[1],
+    #     )
+    #
+    # return None, p_tvt__ntvs
