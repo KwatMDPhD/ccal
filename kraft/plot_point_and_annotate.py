7dc026987e71ea72913cac30f3a948264d81ca24
KwatME <kwatme8@gmail.com>
2019-07-25 01:46:29 -0700

    Ran match_pattern code

---
+++ kraft/plot_point_and_annotate.py
@@ -0,0 +1,100 @@
+from pandas import Series
+
+from .plot_plotly_figure import plot_plotly_figure
+
+
+def plot_point_and_annotate(
+    x,
+    y,
+    abs_dimension,
+    annotation=(),
+    opacity=0.64,
+    annotation_font_size=10,
+    title=None,
+    html_file_path=None,
+):
+
+    if x is None:
+
+        x = Series(range(y.size), name="Rank", index=y.index)
+
+    if abs_dimension == "x":
+
+        is_negative = x < 0
+
+    elif abs_dimension == "y":
+
+        is_negative = y < 0
+
+    data = [
+        {
+            "type": "scatter",
+            "name": "-",
+            "x": x[is_negative],
+            "y": y[is_negative].abs(),
+            "text": y.index[is_negative],
+            "mode": "markers",
+            "marker": {"color": "#0088ff", "opacity": opacity},
+        },
+        {
+            "type": "scatter",
+            "name": "+",
+            "x": x[~is_negative],
+            "y": y[~is_negative],
+            "text": y.index[~is_negative],
+            "mode": "markers",
+            "marker": {"color": "#ff1968", "opacity": opacity},
+        },
+    ]
+
+    annotations = []
+
+    for group_name, elements, size, color in annotation:
+
+        group_elements = y.index & elements
+
+        group_x = x[group_elements]
+
+        group_y = y[group_elements]
+
+        data.append(
+            {
+                "type": "scatter",
+                "name": group_name,
+                "x": group_x,
+                "y": group_y.abs(),
+                "text": group_elements,
+                "mode": "markers",
+                "marker": {
+                    "size": size,
+                    "color": color,
+                    "line": {"width": 1, "color": "#ebf6f7"},
+                },
+            }
+        )
+
+        annotations += [
+            {
+                "x": x_,
+                "y": abs(y_),
+                "text": element,
+                "font": {"size": annotation_font_size},
+                "arrowhead": 2,
+                "arrowsize": 0.8,
+                "clicktoshow": "onoff",
+            }
+            for element, x_, y_ in zip(group_elements, group_x, group_y)
+        ]
+
+    plot_plotly_figure(
+        {
+            "layout": {
+                "title": title,
+                "xaxis": {"title": x.name},
+                "yaxis": {"title": y.name},
+                "annotations": annotations,
+            },
+            "data": data,
+        },
+        html_file_path,
+    )
