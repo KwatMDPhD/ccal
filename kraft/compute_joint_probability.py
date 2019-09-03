cfde4be28b66982ca99c21f8fbb82a6b0d2a4b5c
KwatME <kwatme8@gmail.com>
2019-09-02 21:53:47 -0700

    Time to tackle observation_x_dimensino series....

--- kraft/compute_joint_probability.py
+++ kraft/compute_joint_probability.py
@@ -1,25 +1,27 @@
 from numpy import product
 
-from .estimate_kernel_density import estimate_kernel_density
-from .get_d_dimensions import get_d_dimensions
+from .estimate_element_x_dimension_kernel_density import (
+    estimate_element_x_dimension_kernel_density,
+)
+from .get_element_x_dimension_d_dimensions import get_element_x_dimension_d_dimensions
 from .plot_mesh_grid import plot_mesh_grid
 
 
-def compute_joint_probability(
-    observation_x_dimension,
+def compute_element_x_dimension_joint_probability(
+    element_x_dimension,
     plot=True,
     dimension_names=None,
-    **estimate_kernel_density_keyword_arguments,
+    **estimate_element_x_dimension_kernel_density_keyword_arguments,
 ):
 
-    mesh_grid_point_x_dimension, mesh_grid_point_kernel_density = estimate_kernel_density(
-        observation_x_dimension,
+    mesh_grid_point_x_dimension, mesh_grid_point_kernel_density = estimate_element_x_dimension_kernel_density(
+        element_x_dimension,
         plot=plot,
         dimension_names=dimension_names,
-        **estimate_kernel_density_keyword_arguments,
+        **estimate_element_x_dimension_kernel_density_keyword_arguments,
     )
 
-    d_dimensions = get_d_dimensions(mesh_grid_point_x_dimension)
+    d_dimensions = get_element_x_dimension_d_dimensions(mesh_grid_point_x_dimension)
 
     mesh_grid_point_joint_probability = mesh_grid_point_kernel_density / (
         mesh_grid_point_kernel_density.sum() * product(d_dimensions)
