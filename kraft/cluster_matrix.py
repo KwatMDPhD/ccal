ea17ee85dd5c272bc37207d11f118f1ba13c5c4c
KwatME <kwatme8@gmail.com>
2019-09-02 21:23:50 -0700

    Finished! cluster_matrix

--- kraft/cluster_matrix.py
+++ kraft/cluster_matrix.py
@@ -2,25 +2,14 @@ from numpy import concatenate, where
 from pandas import unique
 from scipy.cluster.hierarchy import dendrogram, linkage
 
-from .apply_function_on_2_vectors import apply_function_on_2_vectors
 from .check_array_for_bad import check_array_for_bad
-
-
-def _ignore_bad_and_compute_euclidean_distance_between_2_vectors(vector_0, vector_1):
-
-    return apply_function_on_2_vectors(
-        vector_0,
-        vector_1,
-        lambda vector_0, vector_1: ((vector_0 - vector_1) ** 2).sum() ** 0.5,
-        raise_for_bad=False,
-    )
 
 
 def cluster_matrix(
     matrix,
     axis,
     groups=None,
-    distance_function=_ignore_bad_and_compute_euclidean_distance_between_2_vectors,
+    distance_function="euclidean",
     linkage_method="average",
     optimal_ordering=True,
     raise_for_bad=True,
