f62e1bec4ee92649be784bc941cdf4036f3d44aa
KwatME <kwatme8@gmail.com>
2019-09-02 22:58:58 -0700

    Next stop: compute_vector_context

--- kraft/compute_element_x_dimension_posterior_probability.py
+++ kraft/compute_element_x_dimension_posterior_probability.py
@@ -1,26 +1,30 @@
 from numpy import apply_along_axis
 
-from .compute_joint_probability import compute_joint_probability
-from .get_d_dimensions import get_d_dimensions
+from .compute_element_x_dimension_joint_probability import (
+    compute_element_x_dimension_joint_probability,
+)
+from .get_element_x_dimension_d_dimensions import get_element_x_dimension_d_dimensions
 from .plot_mesh_grid import plot_mesh_grid
 from .unmesh import unmesh
 
 
-def compute_posterior_probability(
-    observation_x_dimension,
+def compute_element_x_dimension_posterior_probability(
+    element_x_dimension,
     plot=True,
     dimension_names=None,
-    **estimate_kernel_density_keyword_arguments,
+    **estimate_element_x_dimension_kernel_density_keyword_arguments,
 ):
 
-    mesh_grid_point_x_dimension, mesh_grid_point_joint_probability = compute_joint_probability(
-        observation_x_dimension,
+    mesh_grid_point_x_dimension, mesh_grid_point_joint_probability = compute_element_x_dimension_joint_probability(
+        element_x_dimension,
         plot=plot,
         dimension_names=dimension_names,
-        **estimate_kernel_density_keyword_arguments,
+        **estimate_element_x_dimension_kernel_density_keyword_arguments,
     )
 
-    d_target_dimension = get_d_dimensions(mesh_grid_point_x_dimension)[-1]
+    d_target_dimension = get_element_x_dimension_d_dimensions(
+        mesh_grid_point_x_dimension
+    )[-1]
 
     joint_probability = unmesh(
         mesh_grid_point_x_dimension, mesh_grid_point_joint_probability
