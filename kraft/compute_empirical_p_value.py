9a60a12376a6085a67c236cf54f2343280ef297b
KwatME <kwatme8@gmail.com>
2019-09-02 21:31:32 -0700

    compute_empirical_p_value.py

--- kraft/compute_empirical_p_value.py
+++ kraft/compute_empirical_p_value.py
@@ -7,26 +7,26 @@ def compute_empirical_p_value(
     value, random_values, p_value_direction, raise_for_bad=True
 ):
 
-    if isnan(value):
+    is_good = ~check_array_for_bad(random_values, raise_for_bad=raise_for_bad)
+
+    if isnan(value) or not is_good.any():
 
         return nan
 
-    is_good = ~check_array_for_bad(random_values, raise_for_bad=raise_for_bad)
+    random_values_good = random_values[is_good]
 
-    if is_good.any():
+    if p_value_direction == "<":
 
-        random_values_good = random_values[is_good]
+        n_significant_random_value = (random_values_good <= value).sum()
 
-        if p_value_direction == "<":
+    elif p_value_direction == ">":
 
-            n_significant_random_value = (random_values_good <= value).sum()
+        n_significant_random_value = (value <= random_values_good).sum()
 
-        elif p_value_direction == ">":
+    if n_significant_random_value == 0:
 
-            n_significant_random_value = (value <= random_values_good).sum()
-
-        return max(1, n_significant_random_value) / random_values_good.size
+        return 1 / random_values_good
 
     else:
 
-        return nan
+        return n_significant_random_value / random_values_good
