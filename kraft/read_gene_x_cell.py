96eabd77de546c618aedb4ce9cbaeff0ad283bca
KwatME <kwatme8@gmail.com>
2019-09-06 21:30:15 -0700

    Simplified reading gene-x-cell more

--- kraft/read_gene_x_cell.py
+++ kraft/read_gene_x_cell.py
@@ -2,10 +2,17 @@ from pandas import DataFrame, read_csv
 from os.path import join
 from scipy.io import mmread
 from .process_feature_x_sample import process_feature_x_sample
+from .get_dataframe_slice_fraction_good import get_dataframe_slice_fraction_good
+from .select_series_indices import select_series_indices
 
 
-def read_gene_x_cell(
-    mtx_file_path, index_file_path, column_file_path, output_directory_path
+def read_process_write_gene_x_cell(
+    mtx_file_path,
+    index_file_path,
+    column_file_path,
+    output_directory_path,
+    minimum_fraction_cell_with_gene_signal=None,
+    minimum_fraction_gene_z_score=-1,
 ):
 
     gene_x_cell = DataFrame(
@@ -49,4 +56,42 @@ def read_gene_x_cell(
 
     gene_x_cell__clean__log.fillna(0).to_csv(
         output_file_path.replace(".tsv", ".clean.log.na0.tsv"), sep="\t"
+    )
+
+    if minimum_fraction_cell_with_gene_signal is None:
+
+        genes = gene_x_cell__clean__log.index
+
+    else:
+
+        genes = select_series_indices(
+            get_dataframe_slice_fraction_good(gene_x_cell__clean__log, 1),
+            ">",
+            thresholds=(minimum_fraction_cell_with_gene_signal,),
+            title={"text": "Genes"},
+            xaxis={"title": {"text": "Ranking"}},
+            yaxis={"title": {"text": "Fraction Cell"}},
+        )
+
+    print("Selected {} genes.".format(genes.size))
+
+    if minimum_fraction_gene_z_score is None:
+
+        cells = gene_x_cell__clean__log.columns
+
+    else:
+
+        cells = select_series_indices(
+            get_dataframe_slice_fraction_good(gene_x_cell__clean__log.loc[genes], 0),
+            ">",
+            standard_deviation=minimum_fraction_gene_z_score,
+            title={"text": "Cells"},
+            xaxis={"title": {"text": "Ranking"}},
+            yaxis={"title": {"text": "Fraction Gene"}},
+        )
+
+    print("Selected {} cells.".format(cells.size))
+
+    gene_x_cell__clean__log.loc[genes, cells].to_csv(
+        output_file_path.replace(".tsv", ".clean.log.selected.tsv"), sep="\t"
     )
