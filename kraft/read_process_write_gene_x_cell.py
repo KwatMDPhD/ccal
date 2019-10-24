b4aade48a354ab6a946ac677192e7289d1901781
KwatME <kwatme8@gmail.com>
2019-10-24 16:56:49 -0700

    read_process_write_gene_x_cell

--- kraft/read_process_write_gene_x_cell.py
+++ kraft/read_process_write_gene_x_cell.py
@@ -3,18 +3,9 @@ from os.path import join
 from pandas import DataFrame, read_csv
 from scipy.io import mmread
 
-from .get_dataframe_fraction_good_on_axis import get_dataframe_fraction_good_on_axis
-from .process_feature_x_sample import process_feature_x_sample
-from .select_series_indices import select_series_indices
-
 
 def read_process_write_gene_x_cell(
-    mtx_file_path,
-    index_file_path,
-    column_file_path,
-    output_directory_path,
-    minimum_fraction_cell_with_gene_signal=None,
-    minimum_fraction_gene_z_score=-1,
+    mtx_file_path, index_file_path, column_file_path, output_directory_path
 ):
 
     gene_x_cell = DataFrame(
@@ -37,54 +28,4 @@ def read_process_write_gene_x_cell(
 
     gene_x_cell = gene_x_cell.sort_index().sort_index(axis=1)
 
-    tsv_file_path = join(output_directory_path, "gene_x_cell.tsv")
-
-    gene_x_cell.to_csv(tsv_file_path, sep="\t")
-
-    gene_x_cell_prepare = process_feature_x_sample(
-        gene_x_cell,
-        nanize=0,
-        min_n_not_na_value=3,
-        shift_as_necessary_to_achieve_min_before_logging=1,
-        log_base=2,
-    )
-
-    gene_x_cell_prepare.to_csv(tsv_file_path.replace(".tsv", "_prepare.tsv"), sep="\t")
-
-    if minimum_fraction_cell_with_gene_signal is None:
-
-        genes = gene_x_cell_prepare.index
-
-    else:
-
-        genes = select_series_indices(
-            get_dataframe_fraction_good_on_axis(gene_x_cell_prepare, 1),
-            ">",
-            thresholds=(minimum_fraction_cell_with_gene_signal,),
-            layout={
-                "title": {"text": "Genes"},
-                "yaxis": {"title": {"text": "Fraction Cell"}},
-            },
-        )
-
-    gene_x_cell_prepare_select_gene = gene_x_cell_prepare.loc[genes]
-
-    if minimum_fraction_gene_z_score is None:
-
-        cells = gene_x_cell_prepare_select_gene.columns
-
-    else:
-
-        cells = select_series_indices(
-            get_dataframe_fraction_good_on_axis(gene_x_cell_prepare_select_gene, 0),
-            ">",
-            standard_deviation=minimum_fraction_gene_z_score,
-            layout={
-                "title": {"text": "Cells"},
-                "yaxis": {"title": {"text": "Fraction Gene"}},
-            },
-        )
-
-    gene_x_cell_prepare_select_gene[cells].to_csv(
-        tsv_file_path.replace(".tsv", "_prepare_select.tsv"), sep="\t"
-    )
+    gene_x_cell.to_csv(join(output_directory_path, "gene_x_cell.tsv"), sep="\t")
