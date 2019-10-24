3ed9dc41ac0df8c679c1a869106502366fb71d55
KwatME <kwatme8@gmail.com>
2019-10-24 00:54:23 -0700

    Normalize automatically by sum

--- kraft/make_element_x_dimension_from_element_x_node_and_node_x_dimension.py
+++ kraft/make_element_x_dimension_from_element_x_node_and_node_x_dimension.py
@@ -1,7 +1,5 @@
 from numpy import full, nan, sort
 
-from .normalize_array_on_axis import normalize_array_on_axis
-
 
 def make_element_x_dimension_from_element_x_node_and_node_x_dimension(
     element_x_node, node_x_dimension, n_pull
@@ -10,10 +8,6 @@ def make_element_x_dimension_from_element_x_node_and_node_x_dimension(
     element_x_dimension = full(
         (element_x_node.shape[0], node_x_dimension.shape[1]), nan
     )
-
-    if 3 < element_x_node.shape[1]:
-
-        element_x_node = normalize_array_on_axis(element_x_node, 1, "0-1")
 
     for element_index in range(element_x_node.shape[0]):
 
