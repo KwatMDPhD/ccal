297da99380c01d2f0fc710bbdac5aee9ad190c12
KwatME <kwatme8@gmail.com>
2019-10-24 19:02:19 -0700

    Compute entropy for only all positive vector

--- kraft/compute_vector_entropy.py
+++ kraft/compute_vector_entropy.py
@@ -3,6 +3,8 @@ from numpy import log
 
 def compute_vector_entropy(vector):
 
+    assert (0 <= vector).all()
+
     probability = vector / vector.sum()
 
     return -(probability * log(probability)).sum()
