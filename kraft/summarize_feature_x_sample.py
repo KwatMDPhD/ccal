e9523fdcfc599828f4a79a72c267593022bc5bf4
KwatME <kwatme8@gmail.com>
2019-10-27 00:08:14 -0700

    plot=False

--- kraft/summarize_feature_x_sample.py
+++ kraft/summarize_feature_x_sample.py
@@ -7,13 +7,14 @@ from .plot_histogram import plot_histogram
 def summarize_feature_x_sample(
     feature_x_sample,
     feature_x_sample_name=None,
+    plot=True,
     plot_heat_map_max_size=1e6,
     plot_histogram_max_size=1e3,
 ):
 
     print("Shape: {}".format(feature_x_sample.shape))
 
-    if feature_x_sample.size <= plot_heat_map_max_size:
+    if plot and feature_x_sample.size <= plot_heat_map_max_size:
 
         plot_heat_map(
             feature_x_sample, layout={"title": {"text": feature_x_sample_name}}
@@ -29,34 +30,36 @@ def summarize_feature_x_sample(
 
     print("Not-NA max: {:.2e}".format(feature_x_sample_not_na_values.max()))
 
-    if plot_histogram_max_size < feature_x_sample_not_na_values.size:
+    if plot:
 
-        print(
-            "Sampling random {:.2e} values for histogram...".format(
-                plot_histogram_max_size
+        if plot_histogram_max_size < feature_x_sample_not_na_values.size:
+
+            print(
+                "Sampling random {:.2e} values for histogram...".format(
+                    plot_histogram_max_size
+                )
             )
-        )
 
-        feature_x_sample_not_na_values = feature_x_sample_not_na_values[
-            choice(
-                feature_x_sample_not_na_values.index,
-                size=int(plot_histogram_max_size),
-                replace=False,
-            ).tolist()
-            + [
-                feature_x_sample_not_na_values.idxmin(),
-                feature_x_sample_not_na_values.idxmax(),
+            feature_x_sample_not_na_values = feature_x_sample_not_na_values[
+                choice(
+                    feature_x_sample_not_na_values.index,
+                    size=int(plot_histogram_max_size),
+                    replace=False,
+                ).tolist()
+                + [
+                    feature_x_sample_not_na_values.idxmin(),
+                    feature_x_sample_not_na_values.idxmax(),
+                ]
             ]
-        ]
 
-    plot_histogram(
-        (feature_x_sample_not_na_values,),
-        plot_rug=False,
-        layout={
-            "title": {"text": feature_x_sample_name},
-            "xaxis": {"title": {"text": "Not-NA Value"}},
-        },
-    )
+        plot_histogram(
+            (feature_x_sample_not_na_values,),
+            plot_rug=False,
+            layout={
+                "title": {"text": feature_x_sample_name},
+                "xaxis": {"title": {"text": "Not-NA Value"}},
+            },
+        )
 
     feature_x_sample_isna = feature_x_sample.isna()
 
@@ -72,15 +75,17 @@ def summarize_feature_x_sample(
 
         sample_n_na.name = feature_x_sample_isna.columns.name
 
-        plot_histogram(
-            (feature_n_na, sample_n_na),
-            plot_rug=False,
-            layout={
-                "title": {
-                    "text": "{}<br>Fraction NA: {:.2e}".format(
-                        feature_x_sample_name, n_na / feature_x_sample.size
-                    )
+        if plot:
+
+            plot_histogram(
+                (feature_n_na, sample_n_na),
+                plot_rug=False,
+                layout={
+                    "title": {
+                        "text": "{}<br>Fraction NA: {:.2e}".format(
+                            feature_x_sample_name, n_na / feature_x_sample.size
+                        )
+                    },
+                    "xaxis": {"title": {"text": "N NA"}},
                 },
-                "xaxis": {"title": {"text": "N NA"}},
-            },
-        )
+            )
