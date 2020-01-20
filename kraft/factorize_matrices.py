127bba15548bdbcd09a98bcd100ff8aec105d391
KwatME <kwatme8@gmail.com>
2020-01-19 21:46:38 -0800

    tackle

--- kraft/factorize_matrices.py
+++ kraft/factorize_matrices.py
@@ -1,27 +1,128 @@
-from numpy import isnan
+from numpy import asarray, full, isnan, nan, sum
+from numpy.linalg import norm
+from numpy.random import random_sample, seed
 from pandas import DataFrame, Index
 
-from .mf_vs_w_hs import mf_vs_w_hs
-from .mf_vs_ws_h import mf_vs_ws_h
 from .plot_errors import plot_errors
-from .plot_mf import plot_mf
+from .plot_matrix_factorization import plot_matrix_factorization
+from .RANDOM_SEED import RANDOM_SEED
+from .update_h_by_multiplicative_update import update_h_by_multiplicative_update
+from .update_w_by_multiplicative_update import update_w_by_multiplicative_update
 
 
 def factorize_matrices(
-    dataframes, method, r, directory_path, plot_w_h=True, **mf_vs_keyword_arguments
+    vs,
+    r,
+    mode,
+    weights=None,
+    tolerance=1e-6,
+    n_iteration=int(1e6),
+    random_seed=RANDOM_SEED,
+    directory_path=None,
+    plot=True,
 ):
 
-    vs = tuple(dataframe.values for dataframe in dataframes)
+    if mode == "ws":
 
-    if method == "vs_ws_h":
+        assert len(set(v.shape[1] for v in vs)) == 1
 
-        ws, h, errors = mf_vs_ws_h(vs, r, **mf_vs_keyword_arguments)
+        n_v = len(vs)
+
+        errors = full((n_v, n_iteration + 1), nan)
+
+        seed(seed=random_seed)
+
+        ws = [random_sample(size=(v.shape[0], r)) for v in vs]
+
+        h = random_sample(size=(r, vs[0].shape[1]))
+
+        errors[:, 0] = [norm(vs[i] - ws[i] @ h) for i in range(n_v)]
+
+        v_0_norm = norm(vs[0])
+
+        if weights is None:
+
+            weights = [v_0_norm / norm(v) for v in vs]
+
+        n_per_print = max(1, n_iteration // 10)
+
+        for j in range(n_iteration):
+
+            if j % n_per_print == 0:
+
+                print("(r={}) {}/{}...".format(r, j + 1, n_iteration))
+
+            top = sum([weights[i] * ws[i].T @ vs[i] for i in range(n_v)], axis=0)
+
+            bottom = sum([weights[i] * ws[i].T @ ws[i] @ h for i in range(n_v)], axis=0)
+
+            h *= top / bottom
+
+            ws = [
+                update_w_by_multiplicative_update(vs[i], ws[i], h) for i in range(n_v)
+            ]
+
+            j_1_errors = asarray([norm(vs[i] - ws[i] @ h) for i in range(n_v)])
+
+            errors[:, j + 1] = j_1_errors
+
+            j_errors = errors[:, j]
+
+            if (((j_errors - j_1_errors) / j_errors) < tolerance).all():
+
+                break
 
         hs = (h,)
 
-    elif method == "vs_w_hs":
+    elif mode == "hs":
 
-        w, hs, errors = mf_vs_w_hs(vs, r, **mf_vs_keyword_arguments)
+        assert len(set(v.shape[0] for v in vs)) == 1
+
+        n_v = len(vs)
+
+        errors = full((n_v, n_iteration + 1), nan)
+
+        seed(seed=random_seed)
+
+        w = random_sample(size=(vs[0].shape[0], r))
+
+        hs = [random_sample(size=(r, v.shape[1])) for v in vs]
+
+        errors[:, 0] = [norm(vs[i] - w @ hs[i]) for i in range(n_v)]
+
+        v_0_norm = norm(vs[0])
+
+        if weights is None:
+
+            weights = [v_0_norm / norm(v) for v in vs]
+
+        n_per_print = max(1, n_iteration // 100)
+
+        for j in range(n_iteration):
+
+            if j % n_per_print == 0:
+
+                print("(r={}) {}/{}...".format(r, j + 1, n_iteration))
+
+            top = sum([weights[i] * vs[i] @ hs[i].T for i in range(n_v)], axis=0)
+
+            bottom = sum([weights[i] * w @ hs[i] @ hs[i].T for i in range(n_v)], axis=0)
+
+            w *= top / bottom
+
+            hs = [
+                update_h_by_multiplicative_update(vs[i], w, hs[i]) for i in range(n_v)
+            ]
+
+            j_1_errors = asarray([norm(vs[i] - w @ hs[i]) for i in range(n_v)])
+
+            errors[:, j + 1] = j_1_errors
+
+            j_errors = errors[:, j]
+
+            if (((j_errors - j_1_errors) / j_errors) < tolerance).all():
+
+                break
 
         ws = (w,)
 
@@ -29,12 +130,12 @@ def factorize_matrices(
 
     ws = tuple(
         DataFrame(w, index=dataframe.index, columns=index_factors)
-        for dataframe, w in zip(dataframes, ws)
+        for dataframe, w in zip(vs, ws)
     )
 
     hs = tuple(
         DataFrame(h, index=index_factors, columns=dataframe.columns)
-        for dataframe, h in zip(dataframes, hs)
+        for dataframe, h in zip(vs, hs)
     )
 
     for i, w in enumerate(ws):
@@ -45,13 +146,15 @@ def factorize_matrices(
 
         h.to_csv("{}/h{}.tsv".format(directory_path, i), sep="\t")
 
-    if plot_w_h:
+    if plot:
 
-        plot_mf(ws, hs, directory_path)
+        plot_matrix_factorization(ws, hs, directory_path)
 
     plot_errors(
         tuple(errors_[~isnan(errors_)] for errors_ in errors),
-        layout={"title": {"text": "MF method {} r {} ".format(method, r)}},
+        layout={
+            "title": {"text": "Matrix Factorization (mode {} r {})".format(mode, r)}
+        },
     )
 
     return ws, hs
--- kraft/factorize_matrix_by_update.py
+++ kraft/factorize_matrix_by_update.py
@@ -7,9 +7,7 @@ from .update_h_by_multiplicative_update import update_h_by_multiplicative_update
 from .update_w_by_multiplicative_update import update_w_by_multiplicative_update
 
 
-def factorize_matrix_by_update(
-    V, k, n_iteration=int(1e3), random_seed=RANDOM_SEED
-):
+def factorize_matrix_by_update(V, k, n_iteration=int(1e3), random_seed=RANDOM_SEED):
 
     R_norms = full(n_iteration + 1, nan)
 
