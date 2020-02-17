775494ea0edd6a67c63167ff8253990aaed352a0
KwatME <kwatme8@gmail.com>
2020-02-16 20:21:53 -0800

    value_ cdf s1 area

--- kraft/get_s1.py
+++ kraft/get_s1.py
@@ -1,10 +1,10 @@
 from .get_kl import get_kl
 
 
-def get_s1(vector_0, vector_1, vector_reference):
+def get_s1(vector_0, vector_1, vector_reference, weight_0=1, weight_1=1):
 
-    kl_0 = get_kl(vector_0, vector_reference)
+    kl_0 = get_kl(vector_0, vector_reference) * weight_0
 
-    kl_1 = get_kl(vector_1, vector_reference)
+    kl_1 = get_kl(vector_1, vector_reference) * weight_1
 
     return kl_0, kl_1, kl_0 - kl_1
