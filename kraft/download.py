109875e7e5b1d1277d93cd84cdaeb4dfc6d88b8b
KwatME <kwatme8@gmail.com>
2020-03-26 16:34:55 -0700

    Updated download

--- kraft/download.py
+++ kraft/download.py
@@ -2,6 +2,8 @@ from os.path import basename
 from urllib.parse import urlsplit
 from urllib.request import urlretrieve
 
+from requests import get
+
 
 def download(url, directory_path):
 
@@ -9,6 +11,14 @@ def download(url, directory_path):
 
     print("{} ==> {}...".format(url, file_path))
 
-    urlretrieve(url, file_path)
+    if url.startswith("ftp"):
+
+        urlretrieve(url, file_path)
+
+    else:
+
+        with open(file_path, mode="wb") as io:
+
+            io.write(get(url, allow_redirects=True).content)
 
     return file_path
