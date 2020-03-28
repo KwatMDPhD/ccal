c75ee60cf00e7bdf8dc0646f8a4587b4231a8d9c
KwatME <kwatme8@gmail.com>
2020-03-28 14:52:12 -0700

    Break

--- kraft/group.py
+++ kraft/group.py
@@ -4,6 +4,10 @@ def group(dataframe):
 
     print(dataframe.shape)
 
+    if dataframe.shape[0] == 0:
+
+        return dataframe
+
     dataframe = dataframe.groupby(level=0).median()
 
     print(dataframe.shape)
