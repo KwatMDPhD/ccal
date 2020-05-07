84bcbbaa8d5e6853a3e099c1d2fefea107c297ab
KwatME <kwatme8@gmail.com>
2020-05-06 18:59:55 -0700

    vector --> array

--- kraft/array/ignore_nan_and_function_2.py
+++ kraft/array/ignore_nan_and_function_2.py
@@ -2,14 +2,14 @@ from numpy import isnan
 
 
 def ignore_nan_and_function_2(
-    vector_0, vector_1, function, *function_arguments, **function_keyword_arguments,
+    array_0, array_1, function, *function_arguments, **function_keyword_arguments,
 ):
 
-    is_not_nan = ~isnan(vector_0) & ~isnan(vector_1)
+    is_not_nan = ~isnan(array_0) & ~isnan(array_1)
 
     return function(
-        vector_0[is_not_nan],
-        vector_1[is_not_nan],
+        array_0[is_not_nan],
+        array_1[is_not_nan],
         *function_arguments,
         **function_keyword_arguments,
     )
