b9b88616120a996a1c1453c3807e0256514d3661
KwatME <kwatme8@gmail.com>
2020-06-13 15:41:46 -0700

    array

--- kraft/array/log copy.py
+++ kraft/array/log copy.py
@@ -1,38 +1,18 @@
-from numpy import log as loge, log2, log10, nanmin
+from numpy import nanmin
 
 from .error_nan import error_nan
 
 
-def log(array, min_before_logging=None, log_base="e"):
+def set_minimum(array, minimum):
 
     error_nan(array)
 
-    assert (0 < array).all()
+    if minimum == "0<":
 
-    array = array.copy()
+        minimum = array[0 < array].min()
 
-    if min_before_logging is not None:
-
-        if min_before_logging == "0<":
-
-            min_before_logging = array[0 < array].min()
-
-        else:
-
-            assert 0 < min_before_logging
+    else:
 
         array += min_before_logging - nanmin(array)
 
-    if log_base == "2":
-
-        log_ = log2
-
-    elif log_base == "e":
-
-        log_ = loge
-
-    elif log_base == "10":
-
-        log_ = log10
-
-    return log_(array)
+    return
