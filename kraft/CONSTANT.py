0a66ae9029ce15b5511a0074a22c224b00d2978d
KwatME <kwatme8@gmail.com>
2020-07-08 12:35:41 -0700

    setup.py!

--- kraft/CONSTANT.py
+++ kraft/CONSTANT.py
@@ -6,4 +6,4 @@ RANDOM_SEED = 20121020
 
 FLOAT_RESOLUTION = finfo(float).resolution
 
-DATA_DIRECTORY_PATH = "{}/../data".format(dirname(__file__))
+DATA_DIRECTORY_PATH = "{}/data".format(dirname(__file__))
--- setup.py
+++ setup.py
@@ -1,36 +1,10 @@
-from os import walk
+from os import listdir
 
 from setuptools import setup
 
+from kraft.CONSTANT import DATA_DIRECTORY_PATH
+
 name = "kraft"
-
-data_directory_name = "data"
-
-
-def get_child_paths(parent_directory_path, relative=True):
-
-    child_paths = []
-
-    for directory_path, directory_names, file_names in walk(parent_directory_path):
-
-        for directory_name in directory_names:
-
-            child_paths.append("{}/{}/".format(directory_path, directory_name))
-
-        for file_name in file_names:
-
-            child_paths.append("{}/{}".format(directory_path, file_name))
-
-    if relative:
-
-        n = len(parent_directory_path) + 1
-
-        return tuple(child_path[n:] for child_path in child_paths)
-
-    else:
-
-        return tuple(child_paths)
-
 
 setup(
     name=name,
@@ -48,11 +22,10 @@ setup(
         "click",
     ),
     packages=(name,),
-    include_package_data=True,
     package_data={
         name: tuple(
-            "{}/{}".format(data_directory_name, path)
-            for path in get_child_paths("{}/{}".format(name, data_directory_name))
+            "{}/{}".format(DATA_DIRECTORY_PATH, file_name)
+            for file_name in listdir(path=DATA_DIRECTORY_PATH)
         )
     },
 )
