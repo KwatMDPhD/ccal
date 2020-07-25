c0befb42367c70adb5107d206cbc08b802a29fa5
KwatME <kwat.me@icloud.com>
2020-07-25 15:28:32 -0700

    frame

--- kraft/name_biology.py
+++ kraft/name_biology.py
@@ -4,23 +4,23 @@ from pandas import isna, read_csv
 from .CONSTANT import DATA_DIRECTORY_PATH
 
 
-def get_gene_symbol(column_to_selections=None):
+def select_genes(selection=None):
 
-    if column_to_selections is None:
+    if selection is None:
 
-        column_to_selections = {"Locus group": ("protein-coding gene",)}
+        selection = {"Locus group": ("protein-coding gene",)}
 
     table = read_csv("{}/hgnc_gene_group.tsv.gz".format(DATA_DIRECTORY_PATH), sep="\t")
 
     is_selected = full(table.shape[0], True)
 
-    for label, selections in column_to_selections.items():
+    for label, selection in selection.items():
 
         print(label)
 
         is_selected &= asarray(
             tuple(
-                not isna(object_) and object_ in selections
+                not isna(object_) and object_ in selection
                 for object_ in table.loc[:, label].to_numpy()
             )
         )
@@ -42,37 +42,37 @@ enst_ensg_gene = read_csv(
     "{}/enst_ensg_gene.tsv.gz".format(DATA_DIRECTORY_PATH), sep="\t"
 )
 
-ENS_GENE = {}
+ens_to_gene = {}
 
 for column in ("Transcript stable ID version", "Gene stable ID version"):
 
     ens_gene = enst_ensg_gene[[column, "Gene name"]].dropna()
 
-    ENS_GENE.update(dict(zip(ens_gene.iloc[:, 0], ens_gene.iloc[:, 1])))
+    ens_to_gene.update(dict(zip(ens_gene.iloc[:, 0], ens_gene.iloc[:, 1])))
 
-for ens, gene in tuple(ENS_GENE.items()):
+for ens, gene in tuple(ens_to_gene.items()):
 
     ens = ens.split(sep=".")[0]
 
-    ENS_GENE[ens] = gene
+    ens_to_gene[ens] = gene
 
-    ENS_GENE["{}_at".format(ens)] = gene
+    ens_to_gene["{}_at".format(ens)] = gene
 
 
 refseq_nm_nc_gene = read_csv(
     "{}/refseq_nm_nc_gene.tsv.gz".format(DATA_DIRECTORY_PATH), sep="\t"
 )
 
-REFSEQ_GENE = {}
+refseq_to_gene = {}
 
 for column in ("RefSeq mRNA ID", "RefSeq ncRNA ID"):
 
     refseq_gene = refseq_nm_nc_gene[[column, "Gene name"]].dropna()
 
-    REFSEQ_GENE.update(dict(zip(refseq_gene.iloc[:, 0], refseq_gene.iloc[:, 1])))
+    refseq_to_gene.update(dict(zip(refseq_gene.iloc[:, 0], refseq_gene.iloc[:, 1])))
 
 
-ILMNID_GENE = (
+ilmnid_to_gene = (
     read_csv(
         "{}/HumanMethylation450_15017482_v1-2.csv.gz".format(DATA_DIRECTORY_PATH),
         skiprows=7,
@@ -86,11 +86,11 @@ ILMNID_GENE = (
 )
 
 
-def map_genes(ids):
+def name_genes(ids):
 
     n_gene_genes = {}
 
-    for dict_ in (ENS_GENE, ILMNID_GENE, REFSEQ_GENE):
+    for dict_ in (ens_to_gene, ilmnid_to_gene, refseq_to_gene):
 
         genes = tuple(map(dict_.get, ids))
 
@@ -109,7 +109,7 @@ def map_genes(ids):
         return n_gene_genes[max(n_gene_genes.keys())]
 
 
-def rename_cell_lines(names):
+def name_cell_lines(names):
 
     name_to_rename = read_csv(
         "{}/cell_line_name_rename.tsv.gz".format(DATA_DIRECTORY_PATH),
