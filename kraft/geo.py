8c9f519c65dd4b4f210b8c56635de104c3aa1781
KwatME <kwat.me@icloud.com>
2020-08-14 14:31:56 -0700

    Done CFS

--- kraft/geo.py
+++ kraft/geo.py
@@ -39,112 +39,9 @@ def _make_platform_or_sample_dict(block):
     return dict_
 
 
-def get_gse(gse_id, directory_path, **download_keyword_arguments):
-
-    file_path = download(
-        "ftp://ftp.ncbi.nlm.nih.gov/geo/series/{0}nnn/{1}/soft/{1}_family.soft.gz".format(
-            gse_id[:-3], gse_id
-        ),
-        directory_path,
-        **download_keyword_arguments
-    )
-
-    platforms = {}
-
-    samples = {}
-
-    for block in open(file_path, mode="rt", errors="replace").read().split(sep="\n^"):
-
-        header, block = block.split(sep="\n", maxsplit=1)
-
-        type_ = header.split(sep=" = ", maxsplit=1)[0]
-
-        if type_ in ("PLATFORM", "SAMPLE"):
-
-            dict_ = _make_platform_or_sample_dict(block)
-
-            if type_ == "PLATFORM":
-
-                name = dict_["Platform_geo_accession"]
-
-                print(name)
-
-                dict_["data"] = []
-
-                platforms[name] = dict_
-
-            elif type_ == "SAMPLE":
-
-                name = dict_["Sample_title"]
-
-                print(name)
-
-                if "table" in dict_:
-
-                    values = dict_.pop("table").loc[:, "VALUE"].apply(cast_builtin)
-
-                    if is_numeric_dtype(values):
-
-                        values.name = name
-
-                        platforms[dict_["Sample_platform_id"]]["data"].append(values)
-
-                    else:
-
-                        print("VALUE is not numeric.")
-
-                samples[name] = dict_
-
-    feature_x_sample = DataFrame(data=samples)
-
-    feature_x_sample.index.name = "Feature"
-
-    _x_samples = (
-        feature_x_sample,
-        *separate_type(
-            feature_x_sample.loc[
-                (
-                    label[:22] == "Sample_characteristics"
-                    for label in feature_x_sample.index.to_numpy()
-                ),
-                :,
-            ],
-            prefix_feature=False,
-        ),
-    )
-
-    for _x_sample in _x_samples:
-
-        if _x_sample is not None:
-
-            peak(_x_sample)
-
-    for platform, dict_ in platforms.items():
-
-        data = dict_.pop("data")
-
-        if 0 < len(data):
-
-            _x_sample = DataFrame(data=data).T
-
-            _x_sample.index = _name_features(
-                _x_sample.index.to_numpy(), platform, dict_["table"]
-            )
-
-            _x_sample.index = name_genes(_x_sample.index.to_numpy())
-
-            _x_sample.index.name = "Gene"
-
-            _x_sample = collapse(_x_sample)
-
-            peak(_x_sample)
-
-            _x_samples += (_x_sample,)
-
-    return _x_samples
-
-
 def _name_features(features, platform, platform_table):
+
+    print(platform)
 
     platform = int(platform[3:])
 
@@ -202,8 +99,6 @@ def _name_features(features, platform, platform_table):
 
         function = None
 
-    print("Platform {} table columns:".format(platform))
-
     for label_ in platform_table.columns.to_numpy():
 
         if label_ == label:
@@ -231,3 +126,115 @@ def _name_features(features, platform, platform_table):
         summarize(id_to_name)
 
         return asarray(tuple(id_to_name.get(id_) for id_ in features))
+
+
+def get_gse(gse_id, directory_path, **download_keyword_arguments):
+
+    file_path = download(
+        "ftp://ftp.ncbi.nlm.nih.gov/geo/series/{0}nnn/{1}/soft/{1}_family.soft.gz".format(
+            gse_id[:-3], gse_id
+        ),
+        directory_path,
+        **download_keyword_arguments
+    )
+
+    platforms = {}
+
+    samples = {}
+
+    for block in open(file_path, mode="rt", errors="replace").read().split(sep="\n^"):
+
+        header, block = block.split(sep="\n", maxsplit=1)
+
+        type_ = header.split(sep=" = ", maxsplit=1)[0]
+
+        if type_ in ("PLATFORM", "SAMPLE"):
+
+            dict_ = _make_platform_or_sample_dict(block)
+
+            if type_ == "PLATFORM":
+
+                name = dict_["Platform_geo_accession"]
+
+                print(name)
+
+                dict_["data"] = []
+
+                platforms[name] = dict_
+
+            elif type_ == "SAMPLE":
+
+                name = dict_["Sample_title"]
+
+                print(name)
+
+                if "table" in dict_:
+
+                    values = (
+                        dict_.pop("table")
+                        .loc[:, "VALUE"]
+                        .replace("", None)
+                        .apply(cast_builtin)
+                    )
+
+                    if is_numeric_dtype(values):
+
+                        values.name = name
+
+                        platforms[dict_["Sample_platform_id"]]["data"].append(values)
+
+                    else:
+
+                        print("VALUE is not numeric:")
+
+                        print(values.head())
+
+                samples[name] = dict_
+
+    feature_x_sample = DataFrame(data=samples)
+
+    feature_x_sample.index.name = "Feature"
+
+    _x_samples = (
+        feature_x_sample,
+        *separate_type(
+            feature_x_sample.loc[
+                (
+                    label[:22] == "Sample_characteristics"
+                    for label in feature_x_sample.index.to_numpy()
+                ),
+                :,
+            ],
+            prefix_feature=False,
+        ),
+    )
+
+    for _x_sample in _x_samples:
+
+        if _x_sample is not None:
+
+            peak(_x_sample)
+
+    for platform, dict_ in platforms.items():
+
+        data = dict_.pop("data")
+
+        if 0 < len(data):
+
+            _x_sample = DataFrame(data=data).T
+
+            _x_sample.index = _name_features(
+                _x_sample.index.to_numpy(), platform, dict_["table"]
+            )
+
+            _x_sample.index = name_genes(_x_sample.index.to_numpy())
+
+            _x_sample.index.name = "Gene"
+
+            _x_sample = collapse(_x_sample)
+
+            peak(_x_sample)
+
+            _x_samples += (_x_sample,)
+
+    return _x_samples
