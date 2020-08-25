98116eb5db03cdaf3597f7cb31a2831c6e30186a
KwatME <kwat.me@icloud.com>
2020-08-25 12:16:31 -0700

    array

--- kraft/array.py
+++ kraft/array.py
@@ -20,6 +20,8 @@ from scipy.stats import rankdata
 
 from .CONSTANT import RANDOM_SEED
 
+# ==============================================================================
+
 
 def check_is_in(array_1d, lookup_array_1d):
 
@@ -47,6 +49,66 @@ def map_integer(array_1d):
     return value_to_integer, integer_to_value
 
 
+# ==============================================================================
+
+
+def check_is_all_sorted(vector):
+
+    difference_ = diff(vector)
+
+    return (difference_ <= 0).all() or (0 <= difference_).all()
+
+
+def check_is_extreme(vector, direction, low_and_high=(), n=0, standard_deviation=0):
+
+    if len(low_and_high) == 2:
+
+        low, high = low_and_high
+
+    elif 0 < n:
+
+        if n < 1:
+
+            low = quantile(vector, n)
+
+            high = quantile(vector, 1 - n)
+
+        else:
+
+            vector_sorted = sort(vector)
+
+            low = vector_sorted[n - 1]
+
+            high = vector_sorted[-n]
+
+    elif 0 < standard_deviation:
+
+        mean = vector.mean()
+
+        margin = vector.std() * standard_deviation
+
+        low = mean - margin
+
+        high = mean + margin
+
+    if direction == "<>":
+
+        is_ = logical_or(vector <= low, high <= vector)
+
+    elif direction == "<":
+
+        is_ = vector <= low
+
+    elif direction == ">":
+
+        is_ = high <= vector
+
+    return is_
+
+
+# ==============================================================================
+
+
 def shuffle(array_2d, random_seed=RANDOM_SEED):
 
     array_2d_copy = array_2d.copy()
@@ -60,25 +122,31 @@ def shuffle(array_2d, random_seed=RANDOM_SEED):
     return array_2d_copy
 
 
-def function_on_2_array_2d(array_2d_0, array_2d_1, function):
+# ==============================================================================
 
-    axis_0_size = array_2d_0.shape[0]
 
-    axis_1_size = array_2d_1.shape[0]
+def function_on_2_array_2d(matrix_0, matrix_1, function):
 
-    array_2d = full((axis_0_size, axis_1_size), nan)
+    axis_0_size = matrix_0.shape[0]
+
+    axis_1_size = matrix_1.shape[0]
+
+    matrix = full((axis_0_size, axis_1_size), nan)
 
     for index_0 in range(axis_0_size):
 
-        array_1d_0 = array_2d_0[index_0]
+        array_1d_0 = matrix_0[index_0]
 
         for index_1 in range(axis_1_size):
 
-            array_1d_1 = array_2d_1[index_1]
+            array_1d_1 = matrix_1[index_1]
 
-            array_2d[index_0, index_1] = function(array_1d_0, array_1d_1)
+            matrix[index_0, index_1] = function(array_1d_0, array_1d_1)
 
-    return array_2d
+    return matrix
+
+
+# ==============================================================================
 
 
 def clip(number_array, standard_deviation):
@@ -125,7 +193,7 @@ def normalize(number_array, method, rank_method="average"):
         return rankdata(number_array, method=rank_method).reshape(number_array.shape)
 
 
-def guess_type(number_array, n_max_category=16):
+def guess_type(number_array, maximum_n_category=16):
 
     if all(isinstance(number, integer) for number in number_array.ravel()):
 
@@ -135,69 +203,14 @@ def guess_type(number_array, n_max_category=16):
 
             return "binary"
 
-        elif n_unique <= n_max_category:
+        elif n_unique <= maximum_n_category:
 
             return "categorical"
 
     return "continuous"
 
 
-def check_is_extreme(vector, direction, low_high=None, n=None, standard_deviation=None):
-
-    if low_high is None:
-
-        if n is not None:
-
-            if n < 1:
-
-                low = quantile(vector, n)
-
-                high = quantile(vector, 1 - n)
-
-            else:
-
-                n = min(vector.size, n)
-
-                vector_sorted = sort(vector)
-
-                low = vector_sorted[n - 1]
-
-                high = vector_sorted[-n]
-
-        elif standard_deviation is not None:
-
-            mean = vector.mean()
-
-            margin = vector.std() * standard_deviation
-
-            low = mean - margin
-
-            high = mean + margin
-
-    else:
-
-        low, high = low_high
-
-    if direction == "<>":
-
-        is_ = logical_or(vector <= low, high <= vector)
-
-    elif direction == "<":
-
-        is_ = vector <= low
-
-    elif direction == ">":
-
-        is_ = high <= vector
-
-    return is_
-
-
-def check_is_all_sorted(vector):
-
-    difference_ = diff(vector)
-
-    return (difference_ <= 0).all() or (0 <= difference_).all()
+# ==============================================================================
 
 
 def check_is_not_nan(number_array):
