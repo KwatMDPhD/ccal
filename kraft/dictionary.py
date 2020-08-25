14f292464f7b35d4a9a92dc27a92cd9f6ed2b6bc
KwatME <kwat.me@icloud.com>
2020-08-25 14:42:22 -0700

    dictionary.py

---
+++ kraft/dictionary.py
@@ -0,0 +1,44 @@
+def summarize(dictionary):
+
+    print(
+        "{} keys => {} unique values".format(
+            len(dictionary), len(set(dictionary.values()))
+        )
+    )
+
+
+def merge(dictionary_0, dictionary_1, function=None):
+
+    dictionary = {}
+
+    for key in sorted(dictionary_0.keys() | dictionary_1.keys()):
+
+        if key in dictionary_0 and key in dictionary_1:
+
+            if callable(function):
+
+                dictionary[key] = function(dictionary_0[key], dictionary_1[key])
+
+            else:
+
+                value_0 = dictionary_0[key]
+
+                value_1 = dictionary_1[key]
+
+                if isinstance(value_0, dict) and isinstance(value_1, dict):
+
+                    dictionary[key] = merge(value_0, value_1)
+
+                else:
+
+                    dictionary[key] = value_1
+
+        elif key in dictionary_0:
+
+            dictionary[key] = dictionary_0[key]
+
+        elif key in dictionary_1:
+
+            dictionary[key] = dictionary_1[key]
+
+    return dictionary
