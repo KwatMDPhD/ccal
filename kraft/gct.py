6404a0e36f40802a05d17b5f7a3292b5040c7b6f
KwatME <kwat.me@icloud.com>
2020-08-25 22:46:05 -0700

    gct.py

--- kraft/gct.py
+++ kraft/gct.py
@@ -1,8 +1,8 @@
 from pandas import read_csv
 
 
-def read(gct_file_path):
+def read(file_path):
 
-    return read_csv(gct_file_path, skiprows=2, sep="\t", index_col=0).drop(
+    return read_csv(file_path, skiprows=2, sep="\t", index_col=0).drop(
         labels=["Description"], axis=1
     )
