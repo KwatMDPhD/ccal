d37273582c17922de093f19d157b2c788cba1521
KwatME <kwat.me@icloud.com>
2021-05-30 18:31:15 -0700

    information

--- kraft/information.py
+++ kraft/information.py
@@ -1,91 +1,96 @@
 from numpy import asarray, exp, log, nan, outer, sign, sqrt, unique
 from scipy.stats import pearsonr
 
-from .grid import get_1d_grid, make_1d_grid
+from .grid import make_1d_grid
 from .kernel_density import get_bandwidth
 from .number___ import normalize
 from .probability import get_probability
 
 
-def get_entropy(vector):
+def get_entropy(nu_):
 
-    probability_ = vector / vector.sum()
+    pr_ = nu_ / nu_.sum()
 
-    return -(probability_ * log(probability_)).sum()
-
-
-def get_kld(vector_0, vector_1):
-
-    return vector_0 * log(vector_0 / vector_1)
+    return -(pr_ * log(pr_)).sum()
 
 
-def get_jsd(vector_0, vector_1, reference_vector=None):
+def get_kld(nu1_, nu2_):
 
-    if reference_vector is None:
-
-        reference_vector = (vector_0 + vector_1) / 2
-
-    kld_0_ = get_kld(vector_0, reference_vector)
-
-    kld_1_ = get_kld(vector_1, reference_vector)
-
-    return kld_0_, kld_1_, kld_0_ - kld_1_
+    return nu1_ * log(nu1_ / nu2_)
 
 
-def get_zd(vector_0, vector_1):
+def get_jsd(nu1_, nu2_, nu3_=None):
 
-    kld_0_ = get_kld(vector_0, vector_1)
+    if nu3_ is None:
 
-    kld_1_ = get_kld(vector_1, vector_0)
+        nu3_ = (nu1_ + nu2_) / 2
 
-    return kld_0_, kld_1_, kld_0_ - kld_1_
+    kl1_ = get_kld(nu1_, nu3_)
+
+    kl2_ = get_kld(nu2_, nu3_)
+
+    return kl1_, kl2_, kl1_ - kl2_
 
 
-def get_ic(vector_0, vector_1):
+def get_zd(nu1_, nu2_):
 
-    if unique(vector_0).size == 1 or unique(vector_1).size == 1:
+    kl1_ = get_kld(nu1_, nu2_)
+
+    kl2_ = get_kld(nu2_, nu1_)
+
+    return kl1_, kl2_, kl1_ - kl2_
+
+
+def get_ic(nu1_, nu2_):
+
+    if 1 in [unique(nu1_).size, unique(nu2_).size]:
 
         return nan
 
-    vector_0 = normalize(vector_0, "-0-")
+    nu1_ = normalize(nu1_, "-0-")
 
-    vector_1 = normalize(vector_1, "-0-")
+    nu2_ = normalize(nu2_, "-0-")
 
-    correlation = pearsonr(vector_0, vector_1)[0]
+    pe = pearsonr(nu1_, nu2_)[0]
 
-    bandwidth_factor = 1 - abs(correlation) * 2 / 3
+    ex = 0.1
 
-    nd_grid, nd_probability_vector = get_probability(
-        asarray([vector_0, vector_1]).T,
+    n_co = 24
+
+    co1_ = make_1d_grid(nu1_.min(), nu1_.max(), ex, n_co)
+
+    co2_ = make_1d_grid(nu2_.min(), nu2_.max(), ex, n_co)
+
+    fa = 1 - abs(pe) * 2 / 3
+
+    ba1 = get_bandwidth(nu1_) * fa
+
+    ba2 = get_bandwidth(nu2_) * fa
+
+    pr_ = get_probability(
+        asarray([nu1_, nu2_]).T,
+        ba_=[ba1, ba2],
+        co__=[co1_, co2_],
         pl=False,
-        ba_=[
-            get_bandwidth(vector) * bandwidth_factor for vector in [vector_0, vector_1]
-        ],
-        co__=[
-            make_1d_grid(vector.min(), vector.max(), 0.1, 24)
-            for vector in [vector_0, vector_1]
-        ],
-    )
+    )[1]
 
-    axis_0_grid, axis_1_grid = get_1d_grid(nd_grid)
+    pr_di_di = pr_.reshape([n_co, n_co])
 
-    p01 = nd_probability_vector.reshape([axis_0_grid.size, axis_1_grid.size])
+    re1 = co1_[1] - co1_[0]
 
-    d0 = axis_0_grid[1] - axis_0_grid[0]
+    re2 = co2_[1] - co2_[0]
 
-    d1 = axis_1_grid[1] - axis_1_grid[0]
+    pr1 = pr_di_di.sum(axis=1) * re2
 
-    p0 = p01.sum(axis=1) * d1
+    pr2 = pr_di_di.sum(axis=0) * re1
 
-    p1 = p01.sum(axis=0) * d0
+    jo_ = outer(pr1, pr2).ravel()
 
-    p0p1 = outer(p0, p1)
+    mu = get_kld(pr_, jo_).sum() * re1 * re2
 
-    mi = get_kld(p01.ravel(), p0p1.ravel()).sum() * d0 * d1
-
-    return sqrt(1 - exp(-2 * mi)) * sign(correlation)
+    return sqrt(1 - exp(-2 * mu)) * sign(pe)
 
 
-def get_icd(vector_0, vector_1):
+def get_icd(nu1_, nu2_):
 
-    return (-get_ic(vector_0, vector_1) + 1) / 2
+    return (-get_ic(nu1_, nu2_) + 1) / 2
