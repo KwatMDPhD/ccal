aa8ff1dfd30e522a197b02bb7fcfde3ff1c50df3
KwatME <kwat.me@icloud.com>
2021-05-30 17:14:44 -0700

    tok

--- kraft/__init__.py
+++ kraft/__init__.py
@@ -1,22 +1,20 @@
-from . import (
-    CONSTANT,
-    any_,
-    clustering,
-    dictionary,
-    function_heat_map,
-    gct,
-    gmt,
-    grid,
-    information,
-    json,
-    kernel_density,
-    number_,
-    number___,
-    path,
-    plot,
-    probability,
-    shell,
-    significance,
-    string,
-    support,
-)
+from . import CONSTANT
+from . import any_
+from . import clustering
+from . import dictionary
+from . import function_heat_map
+from . import gct
+from . import gmt
+from . import grid
+from . import information
+from . import json
+from . import kernel_density
+from . import number_
+from . import number___
+from . import path
+from . import plot
+from . import probability
+from . import shell
+from . import significance
+from . import string
+from . import support
