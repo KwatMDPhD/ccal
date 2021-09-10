be97ed830f3959dae5f50525a8688733f5a01413
KwatME <kwat.me@icloud.com>
2021-09-09 20:28:04 -0600

    point

--- kwat/point/map_point.py
+++ kwat/point/map_point.py
@@ -11,11 +11,4 @@ def map_point(di_po_po, n_di, ra=random_seed, **ke_va):
         n_components=n_di, random_state=ra, dissimilarity="precomputed", **ke_va
     ).fit_transform(di_po_po)
 
-    for ie in range(n_di):
-
-        nu_po_di[:, ie] = normalize(nu_po_di[:, ie], "0-1")
-
-    # TODO
-    assert (nu_po_di == apply_along_axis(normalize, 1, nu_po_di, "0-1")).all()
-
-    return nu_po_di
+    return apply_along_axis(normalize, 0, nu_po_di, "0-1")
