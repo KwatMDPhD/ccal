e2382af6261852ae0d7ca28376e6c44e7e25b504
KwatME <kwat.me@icloud.com>
2021-09-14 15:26:24 -0600

    gene & cell_line

--- kwat/cell_line/rename.py
+++ kwat/cell_line/rename.py
@@ -2,22 +2,18 @@ from pandas import read_csv
 
 from ..constant import DATA_DIRECTORY_PATH
 from ..dictionary import clean
-from ._map_broad import _map_broad
-
-
-def _map_broad():
-
-    return clean(read_csv(
-        "{}cell_line_name_rename.tsv.gz".format(DATA_DIRECTORY_PATH),
-        sep="\t",
-        index_col=0,
-        squeeze=True,
-    ).to_dict())
 
 
 def rename(na_):
 
-    na_re = _map_broad()
+    na_re = clean(
+        read_csv(
+            "{}cell_line_name_rename.tsv.gz".format(DATA_DIRECTORY_PATH),
+            sep="\t",
+            index_col=0,
+            squeeze=True,
+        ).to_dict()
+    )
 
     re_ = []
 
